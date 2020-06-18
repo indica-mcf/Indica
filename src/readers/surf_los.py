0749763ca27d8ac41a3d33e5e0e7492993ac6384
Chris MacMackin <chris.macmackin@ukaea.uk>
2020-06-18 15:22:23 +0100

    Add interface for loading LoS data

---
+++ src/readers/surf_los.py
@@ -0,0 +1,40 @@
+"""Routines for getting start and end points of lines of sight using data from
+Surf data.
+
+"""
+
+import numpy as np
+
+
+def read_surf_los(filename: str, pulse: int, instrument: str,
+                  scale: bool = True) -> (np.ndarray, np.ndarray, np.ndarray,
+                                          np.ndarray):
+    """Read beginning and ends of lines of sight from Surf data.
+
+    Optionally scale the lines of sight so they are all the same
+    length as the longest one.
+
+    Parameters
+    ----------
+    filename
+        Name of the file containing the FLUSH data.
+    pulse
+        The pulse number the data is required for.
+    instrument
+        Which instrument to get the lines of sight for.
+    scale
+        If ``True``, change the end points of lines of sight so they are all
+        the same length as the longest one.
+
+    Retruns
+    -------
+    Rstart
+        Major radius for the start of the line of sight for each channel.
+    Rend
+        Major radius for the end of the line of sight for each channel.
+    Zstart
+        Vertical position for the start of the line of sight for each channel.
+    Zend
+        Vertical position for the end of the line of sight for each channel.
+    """
+    pass
