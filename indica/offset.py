e765431c12457be7e5c12b4e2fafcbec081205a6
Chris MacMackin <chris.macmackin@ukaea.uk>
2020-11-30 22:55:08 +0000

    Wrote initial implementation of interface to choose R-offset

--- indica/offset.py
+++ indica/offset.py
@@ -1,10 +1,13 @@
 """Callback functions to choose an offset for equilibrium data."""
 
 from typing import Callable
+from typing import cast
 from typing import Optional
 from typing import Tuple
 
+import matplotlib.pyplot as plt
 from xarray import DataArray
+from xarray import Dataset
 
 OffsetPicker = Callable[
     [float, DataArray, DataArray, Optional[DataArray]], Tuple[float, bool]
@@ -45,4 +48,59 @@ def interactive_offset_choice(
         the new guess.
 
     """
-    pass
+    print(
+        f"Displaying electron temperatures as a function of normalised magnetic "
+        f"flux\nfor the specified R-offset of {guess}m. Once finished inspecting "
+        "the plot(s),\nclose them."
+    )
+
+    etemps = Dataset(
+        {
+            "T_e": (T_e / 1e3).assign_attrs(
+                long_name=r"Electron Temperature $T_{\rm e}$", units="keV"
+            ),
+            "rho_poloidal": flux.assign_attrs(
+                long_name=r"Poloidal Magnetic Flux $\rho_{\rm pol}$"
+            ),
+        }
+    ).isel(t=slice(None, None, 10))
+    etemps.plot.scatter(x="rho_poloidal", y="T_e")
+    plt.tight_layout()
+    if offset_at_time is not None:
+        plt.figure()
+        offset_at_time = cast(
+            DataArray,
+            offset_at_time.assign_attrs(long_name="Optimal R-offset", units="m"),
+        )
+        offset_at_time.plot()
+        plt.xlabel("Time [s]")
+        plt.tight_layout()
+    plt.show()
+    accept = ask_user(f"Use R-offset of {guess}m?")
+    if not accept:
+        while True:
+            try:
+                new_guess = float(input("Specify a new R-offset: "))
+                break
+            except ValueError:
+                print("Please enter a valid floating-point number.")
+                continue
+    else:
+        new_guess = guess
+    return new_guess, accept
+
+
+def ask_user(question: str) -> bool:
+    check = str(input(f"{question} (Y/N): ")).lower().strip()
+    try:
+        if check[0] == "y":
+            return True
+        elif check[0] == "n":
+            return False
+        else:
+            print("Invalid Input")
+            return ask_user(question)
+    except Exception as error:
+        print("Please enter valid inputs")
+        print(error)
+        return ask_user(question)
