44000ffa05b4cc8d6d3747c5848e15fb9fa21a10
Chris MacMackin <chris.macmackin@ukaea.uk>
2021-02-25 13:57:57 +0000

    Implemented determining hash of indica version for provenance

--- tests/unit/test_session.py
+++ tests/unit/test_session.py
@@ -1,15 +1,19 @@
 """Check PROV/session tracking functionality."""
 
+from pathlib import Path
 from unittest import mock
 
+from hypothesis import assume
 from hypothesis import given
 from hypothesis.strategies import composite
 from hypothesis.strategies import dictionaries
 from hypothesis.strategies import emails
+from hypothesis.strategies import from_regex
 from hypothesis.strategies import lists
 from hypothesis.strategies import text
 import prov.model as prov
 
+import indica
 from indica import session
 from indica.session import hash_vals
 from indica.session import Session
@@ -68,11 +72,94 @@ def test_session_context_manager():
     assert session.global_session == old_session
 
 
-# Check session behaves properly as context manager
+@given(emails())
+def test_session_begin(email):
+    old_global = session.global_session
+    try:
+        Session.begin(email)
+        assert session.global_session != old_global
+        assert session.global_session._user[0].identifier.localpart == email
+    finally:
+        session.global_session = old_global
 
-# Try pushing/popping/accessing PROV agents (RuleBasedStateMachine)
 
-# Test begin() factory method
+@given(
+    emails(), lists(from_regex("[a-z0-9]+", fullmatch=True), unique=True, min_size=1)
+)
+def test_push_pop_agents(email, agent_ids):
+    assume(all(ident != email for ident in agent_ids))
+    session = Session(email)
+    agents = []
+    original_agent = session._user[0]
+    for ident in agent_ids:
+        agents.append(session.prov.agent(ident))
+        session.push_agent(agents[-1])
+        assert session.agent is agents[-1]
+    for agent in agents[::-1]:
+        popped = session.pop_agent()
+        assert popped is agent
+        delegation = next(
+            iter(
+                filter(
+                    lambda x: x.get_attribute("prov:delegate") == {agent.identifier},
+                    session.prov.get_records(prov.ProvDelegation),
+                )
+            )
+        )
+        assert delegation.get_attribute("prov:responsible") == {
+            session.agent.identifier
+        }
+    assert session.agent is original_agent
+
+
+@given(emails(), from_regex("[a-z0-9]+", fullmatch=True))
+def test_new_agent_context(email, agent_id):
+    assume(email != agent_id)
+    session = Session(email)
+    agent = session.prov.agent(agent_id)
+    old_agent = session.agent
+    with session.new_agent(agent):
+        assert session.agent is agent
+        delegation = next(
+            iter(
+                filter(
+                    lambda x: x.get_attribute("prov:delegate") == {agent.identifier},
+                    session.prov.get_records(prov.ProvDelegation),
+                )
+            )
+        )
+        assert delegation.get_attribute("prov:responsible") == {old_agent.identifier}
+    assert session.agent is old_agent
+
+
+def test_dependency_provenance():
+    session = Session("rand.m.person@ukaea.uk")
+    dependencies = list(
+        filter(
+            lambda x: x.get_attribute("prov:generatedEntity")
+            == {session.indica_prov.identifier},
+            session.prov.get_records(prov.ProvDerivation),
+        )
+    )
+    package_dirs = [
+        Path(next(iter(dep.get_attribute("prov:usedEntity"))).localpart)
+        for dep in dependencies
+    ]
+    package_names = [path.name for path in package_dirs]
+    assert len(session.prov.get_record("pypi:indica")) == 1
+    assert "xarray" in package_names
+    assert "scipy" in package_names
+    assert "numpy" in package_names
+    assert "prov" in package_names
+    assert "sal" in package_names
+    assert "matplotlib" in package_names
+    assert "netCDF4" in package_names
+    assert session.prov.get_record("pypi:pandas")
+    indica_records = session.prov.get_record(f"local:{Path(indica.__file__).parent}")
+    commit = indica_records[0].get_attribute("git_commit")
+    assert len(commit) == 1
+    assert "UNKNOWN" not in commit
+
 
 # Test exporting/reloading data produces same result
 
