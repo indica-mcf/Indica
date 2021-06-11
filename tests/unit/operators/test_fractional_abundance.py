502910bc7c2d6e2a76e59c38c18227bc39d49a96
Sanket Gadgil <sanket.gadgil@ukaea.uk>
2021-06-11 12:07:28 +0100

    More unit tests and error checking in FractionalAbundance class initialization

--- tests/unit/operators/test_fractional_abundance.py
+++ tests/unit/operators/test_fractional_abundance.py
@@ -1,9 +1,21 @@
+import unittest
+
 import numpy as np
 import pytest
 from xarray import DataArray
 
 from indica.operators.fractional_abundance import FractionalAbundance
 from indica.readers import ADASReader
+
+
+class Assertion_Test_Case(unittest.TestCase):
+    def init_assert_check(self, SCD, ACD, CCD, PLT, PRC, PRB, Ne, Nh, Te):
+        with self.assertRaises(AssertionError):
+            FractionalAbundance(SCD, ACD, CCD, PLT, PRC, PRB, Ne, Nh, Te)
+
+    def tau_check(self, FracAbundObj: FractionalAbundance, tau):
+        with self.assertRaises(AssertionError):
+            FracAbundObj.calc_N_z_t(tau)
 
 
 @pytest.fixture
@@ -20,9 +32,9 @@ def test_init():
     PRC = ADAS_file.get_adf11("prc", element, "89")
     PRB = ADAS_file.get_adf11("prb", element, "89")
 
-    input_Te = np.power(10.0, np.linspace(4.7, 2, 10))
-    input_Ne = np.power(10.0, np.linspace(19.0, 16.0, 10))
+    input_Ne = np.logspace(19.0, 16.0, 10)
     input_Nh = 1e-5 * input_Ne
+    input_Te = np.logspace(4.6, 2, 10)
 
     input_Te = DataArray(
         data=input_Te, coords={"rho": np.linspace(0.0, 1.0, 10)}, dims=["rho"]
@@ -38,8 +50,113 @@ def test_init():
         example_frac_abundance = FractionalAbundance(
             SCD, ACD, CCD, PLT, PRC, PRB, Ne=input_Ne, Nh=input_Nh, Te=input_Te
         )
-    except Exception:
-        raise Exception("Cannot initialize FractionalAbundance class")
+    except Exception as e:
+        raise e
+
+    test_case = Assertion_Test_Case()
+
+    input_Ne_invalid = np.logspace(30.0, 16.0, 10)
+    input_Ne_invalid = DataArray(
+        data=input_Ne_invalid, coords={"rho": np.linspace(0.0, 1.0, 10)}, dims=["rho"]
+    )
+
+    test_case.init_assert_check(
+        SCD, ACD, CCD, PLT, PRC, PRB, Ne=input_Ne_invalid, Nh=input_Nh, Te=input_Te
+    )
+
+    input_Ne_invalid.data = np.logspace(19.0, 5.0, 10)
+    test_case.init_assert_check(
+        SCD, ACD, CCD, PLT, PRC, PRB, Ne=input_Ne_invalid, Nh=input_Nh, Te=input_Te
+    )
+
+    input_Ne_invalid.data = np.logspace(19.0, 16.0, 10)
+    input_Nh_invalid = np.inf + np.zeros(input_Ne_invalid.data.shape)
+    input_Nh_invalid = DataArray(
+        data=input_Nh_invalid, coords={"rho": np.linspace(0.0, 1.0, 10)}, dims=["rho"]
+    )
+
+    test_case.init_assert_check(
+        SCD,
+        ACD,
+        CCD,
+        PLT,
+        PRC,
+        PRB,
+        Ne=input_Ne_invalid,
+        Nh=input_Nh_invalid,
+        Te=input_Te,
+    )
+
+    input_Nh_invalid.data = -np.inf + np.zeros(input_Ne_invalid.data.shape)
+    test_case.init_assert_check(
+        SCD,
+        ACD,
+        CCD,
+        PLT,
+        PRC,
+        PRB,
+        Ne=input_Ne_invalid,
+        Nh=input_Nh_invalid,
+        Te=input_Te,
+    )
+
+    input_Nh_invalid.data = -1 + np.zeros(input_Ne_invalid.data.shape)
+    test_case.init_assert_check(
+        SCD,
+        ACD,
+        CCD,
+        PLT,
+        PRC,
+        PRB,
+        Ne=input_Ne_invalid,
+        Nh=input_Nh_invalid,
+        Te=input_Te,
+    )
+
+    input_Nh_invalid.data = np.nan + np.zeros(input_Ne_invalid.data.shape)
+    test_case.init_assert_check(
+        SCD,
+        ACD,
+        CCD,
+        PLT,
+        PRC,
+        PRB,
+        Ne=input_Ne_invalid,
+        Nh=input_Nh_invalid,
+        Te=input_Te,
+    )
+
+    input_Nh_invalid.data = 1e-5 * np.copy(input_Ne)
+
+    input_Te_invalid = np.logspace(5, 2, 10)
+    input_Te_invalid = DataArray(
+        data=input_Te_invalid, coords={"rho": np.linspace(0.0, 1.0, 10)}, dims=["rho"]
+    )
+
+    test_case.init_assert_check(
+        SCD,
+        ACD,
+        CCD,
+        PLT,
+        PRC,
+        PRB,
+        Ne=input_Ne_invalid,
+        Nh=input_Nh,
+        Te=input_Te_invalid,
+    )
+
+    input_Te_invalid.data = np.logspace(4.6, -1, 10)
+    test_case.init_assert_check(
+        SCD,
+        ACD,
+        CCD,
+        PLT,
+        PRC,
+        PRB,
+        Ne=input_Ne_invalid,
+        Nh=input_Nh,
+        Te=input_Te_invalid,
+    )
 
     return example_frac_abundance
 
@@ -48,8 +165,8 @@ def test_init():
 def test_interpolate_rates(test_init):
     try:
         SCD_spec, ACD_spec, CCD_spec, num_of_stages = test_init.interpolate_rates()
-    except Exception:
-        raise Exception("Cannot interpolate ionisation rates")
+    except Exception as e:
+        raise e
 
     assert num_of_stages == 5
 
@@ -74,8 +191,8 @@ def test_calc_ionisation_balance_matrix(test_interpolate_rates):
         ionisation_balance_matrix = (
             test_interpolate_rates.calc_ionisation_balance_matrix()
         )
-    except Exception:
-        raise Exception("Cannot calculate ionisation balance matrix")
+    except Exception as e:
+        raise e
 
     assert ionisation_balance_matrix.shape == (5, 5, 10)
 
@@ -90,8 +207,8 @@ def test_calc_ionisation_balance_matrix(test_interpolate_rates):
 def test_calc_N_z_tinf(test_calc_ionisation_balance_matrix):
     try:
         N_z_tinf = test_calc_ionisation_balance_matrix.calc_N_z_tinf()
-    except Exception:
-        raise Exception("Cannot calculate N_z_tinf")
+    except Exception as e:
+        raise e
 
     assert N_z_tinf.shape == (5, 10)
 
@@ -118,8 +235,8 @@ def test_calc_N_z_tinf(test_calc_ionisation_balance_matrix):
 def test_calc_eigen_vals_and_vecs(test_calc_N_z_tinf):
     try:
         eig_vals, eig_vecs = test_calc_N_z_tinf.calc_eigen_vals_and_vecs()
-    except Exception:
-        raise Exception("Cannot calculate eigenvalues and eigenvectors")
+    except Exception as e:
+        raise e
 
     assert eig_vals.shape == (5, 10)
     assert eig_vecs.shape == (5, 5, 10)
@@ -148,8 +265,8 @@ def test_calc_eigen_vals_and_vecs(test_calc_N_z_tinf):
 def test_calc_eigen_coeffs(test_calc_eigen_vals_and_vecs):
     try:
         eig_coeffs, N_z_t0 = test_calc_eigen_vals_and_vecs.calc_eigen_coeffs()
-    except Exception:
-        raise Exception("Cannot calculate coefficients for time evolution equation")
+    except Exception as e:
+        raise e
 
     assert eig_coeffs.shape == (5, 10)
     assert N_z_t0.shape == (5, 10)
@@ -165,11 +282,22 @@ def test_calc_eigen_coeffs(test_calc_eigen_vals_and_vecs):
 
 @pytest.fixture
 def test_calc_N_z_t(test_calc_eigen_coeffs):
+    test_case = Assertion_Test_Case()
+
+    tau = -1
+    test_case.tau_check(test_calc_eigen_coeffs, tau)
+
+    tau = np.inf
+    test_case.tau_check(test_calc_eigen_coeffs, tau)
+
+    tau = -np.inf
+    test_case.tau_check(test_calc_eigen_coeffs, tau)
+
     tau = 1e-16
     try:
         N_z_t = test_calc_eigen_coeffs.calc_N_z_t(tau)
-    except Exception:
-        raise Exception("Cannot calculate the ionisation balance at time tau")
+    except Exception as e:
+        raise e
 
     assert N_z_t.shape == (5, 10)
 
@@ -182,8 +310,8 @@ def test_calc_N_z_t(test_calc_eigen_coeffs):
 
     try:
         N_z_t = test_calc_eigen_coeffs.calc_N_z_t(tau)
-    except Exception:
-        raise Exception("Cannot calculate the ionisation balance at time tau")
+    except Exception as e:
+        raise e
 
     assert np.all(np.logical_not(np.isnan(N_z_t)))
     assert np.all(np.logical_not(np.isinf(N_z_t)))
@@ -203,8 +331,8 @@ def test_calc_N_z_t(test_calc_eigen_coeffs):
 def test_interpolate_power(test_calc_N_z_t):
     try:
         PLT_spec, PRC_spec, PRB_spec = test_calc_N_z_t.interpolate_power()
-    except Exception:
-        raise Exception("Cannot interpolate power coefficients")
+    except Exception as e:
+        raise e
 
     assert PLT_spec.shape == (4, 10)
     assert PRC_spec.shape == (4, 10)
@@ -224,8 +352,8 @@ def test_interpolate_power(test_calc_N_z_t):
 def test_calc_cooling_factor(test_interpolate_power):
     try:
         cooling_factor = test_interpolate_power.calc_cooling_factor()
-    except Exception:
-        raise Exception("Cannot calculate cooling factor for given element")
+    except Exception as e:
+        raise e
 
     assert cooling_factor.shape == (10,)
 
