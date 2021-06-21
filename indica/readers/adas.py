296374ecfcd373cab364ba398e8f0d4f01596723
Marco Sertoli <marco.sertoli@outlook.com>
2021-06-21 11:36:05 +0100

    Fixed missing imports

--- indica/readers/adas.py
+++ indica/readers/adas.py
@@ -20,6 +20,7 @@ from ..datatypes import ADF11_GENERAL_DATATYPES
 from ..datatypes import ADF15_GENERAL_DATATYPES
 from ..datatypes import ORDERED_ELEMENTS
 
+
 # TODO: Evaluate this location
 DEFAULT_PATH = Path("")
 CACHE_DIR = ".indica"
