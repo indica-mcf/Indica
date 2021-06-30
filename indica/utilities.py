dc4a73560bbfee1433c4f3abec62ffb370f5c91a
Chris MacMackin <chris.macmackin@ukaea.uk>
2021-06-30 21:30:10 +0100

    Fix spline interpolation involving the time axis
    Previous implementation couldn't handle when the coordinates onto
    which the interpolation was performed include `t`, if `t` was also a
    coordinate in the data being interpolated. This was down to a mistake
    in the implementation.

--- indica/utilities.py
+++ indica/utilities.py
@@ -158,12 +158,12 @@ def broadcast_spline(
             spline,
             interp_coord,
             input_core_dims=[[]],
-            output_core_dims=[tuple(d if d != "t" else "__new_t" for d in spline_dims)],
-        ).assign_coords(__new_t=spline_coords["t"])
+            output_core_dims=[tuple(d if d != "t" else "__old_t" for d in spline_dims)],
+        ).assign_coords(__old_t=coord_array(spline_coords["t"].data, "__old_t"))
         result = time_outer_product.indica.interp2d(
-            __new_t=interp_coord.coords["t"], method="cubic"
+            __old_t=interp_coord.coords["t"], method="cubic"
         ).assign_coords({k: v for k, v in spline_coords.items() if k != "t"})
-        del result.coords["__new_t"]
+        del result.coords["__old_t"]
         return result
     else:
         return apply_ufunc(
