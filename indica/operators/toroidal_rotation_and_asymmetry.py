36bbaf7bc9ad7d49aa128d2672cfeef0c838300d
Sanket Gadgil <sanket.gadgil@ukaea.uk>
2021-07-23 18:31:39 +0100

    All tests for ToroidalRotation and AsymmetryParameter now implemented.

--- indica/operators/toroidal_rotation_and_asymmetry.py
+++ indica/operators/toroidal_rotation_and_asymmetry.py
@@ -16,6 +16,39 @@ from ..datatypes import DataType
 
 
 class ToroidalRotation(Operator):
+    """Calculate the toroidal rotation from asymmetry parameter.
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    toroidal_rotation
+        xarray.DataArray containing toroidal rotation for a given impurity element
+
+    Methods
+    -------
+    input_check(
+        var_name,
+        var_to_check,
+        var_type,
+        ndim_to_check,
+        greater_than_or_equal_zero
+    )
+        Checks the inputted var_to_check to ensure that it is valid.
+
+    __call__(
+        asymmetry_parameters,
+        ion_temperature,
+        main_ion_mass,
+        impurity_masses,
+        mean_charges,
+        Zeff_diag,
+        electron_temp,
+        impurity_element,
+    )
+        Calculates the toroidal_rotation from the asymmetry_parameter.
+    """
 
     ARGUMENT_TYPES: List[Union[DataType, EllipsisType]] = []
 
@@ -37,6 +70,22 @@ class ToroidalRotation(Operator):
         ndim_to_check: Optional[int] = None,
         greater_than_or_equal_zero: Optional[bool] = None,
     ):
+        """Check validity of inputted variable - type check and
+        various value checks(no infinities, greather than (or equal to) 0 or NaNs)
+
+        Parameters
+        ----------
+        var_name
+            Name of variable to check.
+        var_to_check
+            Variable to check.
+        var_type
+            Type to check variable against, eg. DataArray
+        ndim_to_check
+            Integer to check the number of dimensions of the variable.
+        greater_than_or_equal_zero
+            Boolean to check values in variable > 0 or >= 0.
+        """
         try:
             assert isinstance(var_to_check, var_type)
         except AssertionError:
@@ -88,16 +137,67 @@ class ToroidalRotation(Operator):
         electron_temp: DataArray,
         impurity_element: str,
     ):
+        """Calculates the toroidal rotation from the asymmetry parameter.
+
+        Parameters
+        ----------
+        asymmetry_parameters
+            xarray.DataArray containing asymmetry parameters data.
+        ion_temperature
+            xarray.DataArray containing ion temperature data.
+        main_ion_mass
+            xarray.DataArray containing main ion mass data.
+        impurity_masses
+            xarray.DataArray containing data of the masses of all impurity elements.
+        mean_charges
+            xarray.DataArray containing data of the mean chgarges of all
+            impurity elements.
+        Zeff_diag
+            xarray.DataArray containing Z-effective data from diagnostics.
+        electron_temp
+            xarray.DataArray containing electron temperature data.
+        impurity_element
+            Full name of the impurity element.
+
+        Returns
+        -------
+        toroidal_rotation
+            xarray.DataArray containing data for toroidal rotations
+            for the given impurity element
+        """
+        self.input_check(
+            "asymmetry_parameters", asymmetry_parameters, DataArray, 3, True
+        )
+
+        self.input_check("ion_temperature", ion_temperature, DataArray, 3, False)
+
+        self.input_check(
+            "main_ion_mass", main_ion_mass, float, greater_than_or_equal_zero=False
+        )
+
+        self.input_check("impurity_masses", impurity_masses, DataArray, 1, False)
+
+        self.input_check("mean_charges", mean_charges, DataArray, 3, True)
+
+        self.input_check("Zeff_diag", Zeff_diag, DataArray, 2, True)
+
+        self.input_check("electron_temp", electron_temp, DataArray, 2, False)
+
+        self.input_check("impurity_element", impurity_element, str)
+
         asymmetry_parameter = asymmetry_parameters.sel(elements=impurity_element)
         impurity_mass = impurity_masses.sel(elements=impurity_element)
         mean_charge = mean_charges.sel(elements=impurity_element)
+        ion_temperature = ion_temperature.sel(elements=impurity_element)
 
         toroidal_rotation = 2.0 * ion_temperature * asymmetry_parameter
         toroidal_rotation /= impurity_mass * (
             1.0
-            - (mean_charge * main_ion_mass * Zeff_diag * ion_temperature)
+            - (mean_charge * main_ion_mass * Zeff_diag * electron_temp)
             / (impurity_mass * (ion_temperature + Zeff_diag * electron_temp))
         )
+
+        toroidal_rotation = toroidal_rotation ** 0.5
 
         return toroidal_rotation
 
@@ -153,22 +253,21 @@ class AsymmetryParameter(Operator):
         ndim_to_check: Optional[int] = None,
         greater_than_or_equal_zero: Optional[bool] = None,
     ):
-        """Checks the inputted var_to_check to ensure that it is valid.
+        """Check validity of inputted variable - type check and
+        various value checks(no infinities, greather than (or equal to) 0 or NaNs)
 
         Parameters
         ----------
         var_name
-            Name of variable to check, mostly used for error messages
+            Name of variable to check.
         var_to_check
-            Actual variable to check.
+            Variable to check.
         var_type
-            The type of the variable for type checking.
+            Type to check variable against, eg. DataArray
         ndim_to_check
-            Optional: Number of dimensions the variable should have
-            (if multi-dimensional)
+            Integer to check the number of dimensions of the variable.
         greater_than_or_equal_zero
-            Optional: If True then the function checks that the variable is >= 0
-            if False then the function checks that the variable is > 0
+            Boolean to check values in variable > 0 or >= 0.
         """
         try:
             assert isinstance(var_to_check, var_type)
@@ -212,7 +311,7 @@ class AsymmetryParameter(Operator):
 
     def __call__(  # type: ignore
         self,
-        toroidal_rotation: DataArray,
+        toroidal_rotations: DataArray,
         ion_temperature: DataArray,
         main_ion_mass: float,
         impurity_masses: DataArray,
@@ -225,8 +324,8 @@ class AsymmetryParameter(Operator):
 
         Parameters
         ----------
-        toroidal_rotation
-            xarray.DataArray containing toroidal rotation data.
+        toroidal_rotations
+            xarray.DataArray containing toroidal rotations data.
         ion_temperature
             xarray.DataArray containing ion temperature data.
         main_ion_mass
@@ -242,12 +341,38 @@ class AsymmetryParameter(Operator):
             xarray.DataArray containing electron temperature data.
         impurity_element
             Full name of the impurity element.
+
+        Returns
+        -------
+        asymmetry_parameter
+            xarray.DataArray containing data for asymmetry parameters
+            for the given impurity element
         """
+        self.input_check("toroidal_rotations", toroidal_rotations, DataArray, 3, True)
+
+        self.input_check("ion_temperature", ion_temperature, DataArray, 3, False)
+
+        self.input_check(
+            "main_ion_mass", main_ion_mass, float, greater_than_or_equal_zero=False
+        )
+
+        self.input_check("impurity_masses", impurity_masses, DataArray, 1, False)
+
+        self.input_check("mean_charges", mean_charges, DataArray, 3, True)
+
+        self.input_check("Zeff_diag", Zeff_diag, DataArray, 2, True)
+
+        self.input_check("electron_temp", electron_temp, DataArray, 2, False)
+
+        self.input_check("impurity_element", impurity_element, str)
+
         impurity_mass = impurity_masses.sel(elements=impurity_element)
         mean_charge = mean_charges.sel(elements=impurity_element)
+        toroidal_rotations = toroidal_rotations.sel(elements=impurity_element)
+        ion_temperature = ion_temperature.sel(elements=impurity_element)
 
         asymmetry_parameter = (
-            impurity_mass * toroidal_rotation ** 2 / (2.0 * ion_temperature)
+            impurity_mass * (toroidal_rotations ** 2) / (2.0 * ion_temperature)
         )
         asymmetry_parameter *= 1.0 - (
             mean_charge * main_ion_mass * Zeff_diag * electron_temp
