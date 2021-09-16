8fad62f559ef6f1000e002f9efc1495bf80865c9
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2021-09-16 11:17:58 +0100

    Changed defaults function

--- hda/simple_profiles.py
+++ hda/simple_profiles.py
@@ -5,9 +5,9 @@ from xarray import DataArray
 
 
 def default_profiles():
-    el_temp = build_profile(1.5e3, 50, default="temperature")
-    el_dens = build_profile(5.0e19, 1.0e19, default="density")
-    vrot = build_profile(150.0e3, 10.0e3, default="rotation")
+    el_temp, _ = build_profile(1.5e3, 50, default="temperature")
+    el_dens, _ = build_profile(5.0e19, 1.0e19, default="density")
+    vrot, _ = build_profile(150.0e3, 10.0e3, default="rotation")
 
     return el_temp, el_dens, vrot
 
@@ -18,6 +18,7 @@ def build_profile(
     wped=4,
     wcenter=0.4,
     peaking=1.5,
+    peaking2=1,
     y0_fix=False,
     default=None,
     plot=False,
@@ -37,6 +38,9 @@ def build_profile(
         HFHM of gaussian used for central peaking
     peaking
         peaking factor: new central value = y0 * peaking
+    peaking2
+        additional peaking factor for modification of peaked profile
+        new central value = y0 * peaking * peaking2
     y0_fix
         re-scale new profile to have central value = y0
 
@@ -48,13 +52,10 @@ def build_profile(
     def gaussian(x, A, B, x_0, w):
         return (A - B) * np.exp(-((x - x_0) ** 2) / (2 * w ** 2)) + B
 
-    parameters = {
-        "density": (12, 0.5, 1.3),
-        "temperature": (4, 0.4, 1.5),
-        "rotation": (4, 0.4, 1.4),
-    }
     if default is not None:
-        wped, wcenter, peaking = parameters[default]
+        wped, wcenter, peaking = get_defaults(default)
+
+    params = (y0, y1, wped, wcenter, peaking, y0_fix)
 
     xspl = np.linspace(0, 1.0, 30)
 
@@ -66,6 +67,11 @@ def build_profile(
     if peaking > 1:
         sigma = wcenter / (np.sqrt(2 * np.log(2)))
         ycenter = gaussian(x, y0 * (peaking - 1), 0, 0, sigma)
+        y += ycenter
+
+    if peaking2 > 1:
+        sigma = wcenter / (np.sqrt(2 * np.log(2)))
+        ycenter = gaussian(x, y[0] * (peaking2 - 1), 0, 0, sigma)
         y += ycenter
 
     if y0_fix:
@@ -92,4 +98,15 @@ def build_profile(
 
     result = DataArray(yspl, coords=[(coord, xspl)])
 
-    return result
+    return result, params
+
+def get_defaults(default):
+
+    parameters = {
+        "density": (12, 0.4, 1.3),
+        "temperature": (4, 0.4, 1.5),
+        "rotation": (4, 0.4, 1.4),
+    }
+
+    if default in parameters.keys():
+        return parameters[default]
