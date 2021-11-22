6cefebcf062fa4385b920c0abcf0155830249743
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2021-11-22 09:16:43 +0000

    New quantities for XRCS, added bremsstrahlung measurement

--- indica/readers/abstractreader.py
+++ indica/readers/abstractreader.py
@@ -82,10 +82,18 @@ class DataReader(BaseIO):
             "v": ("effective_charge", "plasma"),
         },
         "get_helike_spectroscopy": {
+            "int_w": ("intensity", "spectral_line"),
+            "int_k": ("intensity", "spectral_line"),
+            "int_tot": ("intensity", "spectral_line"),
+            "int_n3": ("intensity", "spectral_line"),
             "te_kw": ("temperature", "electrons"),
             "te_n3w": ("temperature", "electrons"),
             "ti_w": ("temperature", "ions"),
             "ti_z": ("temperature", "ions"),
+            "ampl_w": ("amplitude", "ar_w"),
+        },
+        "get_filters": {
+            "brems": ("intensity", "bremsstrahlung"),
         },
         "get_interferometry": {"ne": ("density", "electrons"),},
         "get_equilibrium": {
@@ -1375,7 +1383,160 @@ class DataReader(BaseIO):
 
         """
         raise NotImplementedError(
-            "{} does not implement a '_get_spectroscopy' "
+            "{} does not implement a '_get_helike_spectroscopy' "
+            "method.".format(self.__class__.__name__)
+        )
+
+
+    def get_filters(
+        self, uid: str, instrument: str, revision: int, quantities: Set[str],
+    ) -> Dict[str, DataArray]:
+        """Reads filtered radiation diodes
+
+        Parameters
+        ----------
+        uid
+            User ID (i.e., which user created this data)
+        instrument
+            Name of the instrument which measured this data
+        revision
+            An object (of implementation-dependent type) specifying what
+            version of data to get. Default is the most recent.
+        quantities
+            Which physical quantitie(s) to read from the database.
+
+        Returns
+        -------
+        :
+            A dictionary containing the requested data.
+
+        """
+        available_quantities = self.available_quantities(instrument)
+        database_results = self._get_filters(
+            uid, instrument, revision, quantities
+        )
+        if len(database_results) == 0:
+            print(f"No data from {uid}.{instrument}:{revision}")
+            return database_results
+
+        times = database_results["times"]
+        transform = LinesOfSightTransform(
+            database_results["Rstart"],
+            database_results["zstart"],
+            database_results["Tstart"],
+            database_results["Rstop"],
+            database_results["zstop"],
+            database_results["Tstop"],
+            f"{instrument}",
+            database_results["machine_dims"],
+        )
+        downsample_ratio = int(
+            np.ceil((len(times) - 1) / (times[-1] - times[0]) / self._max_freq)
+        )
+        coords: Dict[Hashable, Any] = {"t": times}
+        dims = ["t"]
+        if database_results["length"] > 1:
+            dims.append(transform.x1_name)
+            coords[transform.x1_name] = np.arange(database_results["length"])
+        else:
+            coords[transform.x1_name] = 0
+
+        data = {}
+        drop = []
+        for quantity in quantities:
+            if quantity not in available_quantities:
+                raise ValueError(
+                    "{} can not read filtered diode data for quantity {}".format(
+                        self.__class__.__name__, quantity
+                    )
+                )
+            meta = {
+                "datatype": available_quantities[quantity],
+                "error": DataArray(
+                    database_results[quantity + "_error"], coords, dims
+                ).sel(t=slice(self._tstart, self._tend)),
+                "transform": transform,
+            }
+            quant_data = DataArray(
+                database_results[quantity], coords, dims, attrs=meta,
+            ).sel(t=slice(self._tstart, self._tend))
+            if downsample_ratio > 1:
+                quant_data = quant_data.coarsen(
+                    t=downsample_ratio, boundary="trim", keep_attrs=True
+                ).mean()
+                quant_data.attrs["error"] = np.sqrt(
+                    (quant_data.attrs["error"] ** 2)
+                    .coarsen(t=downsample_ratio, boundary="trim", keep_attrs=True)
+                    .mean()
+                    / downsample_ratio
+                )
+            quant_data.name = instrument + "_" + quantity
+            quant_data.attrs["partial_provenance"] = self.create_provenance(
+                "filters",
+                uid,
+                instrument,
+                revision,
+                quantity,
+                database_results[quantity + "_records"],
+                drop,
+            )
+            quant_data.attrs["provenance"] = quant_data.attrs["partial_provenance"]
+            quant_data.attrs["revision"] = database_results["revision"]
+            data[quantity] = quant_data.indica.ignore_data(drop, transform.x1_name)
+        return data
+
+    def _get_filters(
+        self, uid: str, instrument: str, revision: int, quantities: Set[str],
+    ) -> Dict[str, Any]:
+        """Reads filtered radiation diodes
+
+        Parameters
+        ----------
+        uid
+            User ID (i.e., which user created this data)
+        instrument
+            Name of the instrument which measured this data
+        revision
+            An object (of implementation-dependent type) specifying what
+            version of data to get. Default is the most recent.
+        quantities
+            Which physical quantitie(s) to read from the database.
+
+        Returns
+        -------
+        A dictionary containing the following items:
+
+        times : ndarray
+            The times at which measurements were taken
+        machine_dims
+            A tuple describing the size of the Tokamak domain. It should have
+            the form ``((Rmin, Rmax), (zmin, zmax))``.
+
+        For each requested quantity, the following items will also be present:
+
+        <quantity> : ndarray
+            The data itself (first axis is time, second channel)
+        <quantity>_error : ndarray
+            Uncertainty in the data
+        <quantity>_records : List[str]
+            Representations (e.g., paths) for the records in the database used
+            to access data needed for this data.
+        <quantity>_Rstart : ndarray
+            Major radius of start positions for lines of sight for this data.
+        <quantity>_Rstop : ndarray
+            Major radius of stop positions for lines of sight for this data.
+        <quantity>_zstart : ndarray
+            Vertical location of start positions for lines of sight for this data.
+        <quantity>_zstop : ndarray
+            Vertical location of stop positions for lines of sight for this data.
+        <quantity>_Tstart : ndarray
+            Toroidal offset of start positions for lines of sight for this data.
+        <quantity>_Tstop : ndarray
+            Toroidal offset of stop positions for lines of sight for this data.
+
+        """
+        raise NotImplementedError(
+            "{} does not implement a '_get_filters' "
             "method.".format(self.__class__.__name__)
         )
 
