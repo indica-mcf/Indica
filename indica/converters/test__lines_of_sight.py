da772778f25664a0708d9ebe112695fdc8b0456a
jonathan.wood <jonathan.wood@tokamakenergy.co.uk>
2022-03-14 15:37:25 +0000

    Adding setting dell to the line of sight transform class

--- indica/converters/test__lines_of_sight.py
+++ indica/converters/test__lines_of_sight.py
@@ -1,13 +1,9 @@
 import sys
-#sys.path.insert(0, "/../../")
 sys.path.insert(0, "/home/jonathan.wood/git_home/Indica")
-#print(sys.path)
 sys.path.remove("/home/marco.sertoli/python/Indica")
-#print(sys.path)
 from indica.converters import lines_of_sight
-
-
-#from indica.converters import lines_of_sight
+from matplotlib import pyplot as plt
+import numpy as np
 
 # Line of sight origin tuple
 origin = (0.9, -0.1, 0.0)  # [xyz]
@@ -23,3 +19,30 @@ name = "los_test"
 
 # Set-up line of sight class
 los = lines_of_sight.LinesOfSightTransform(origin, direction, machine_dimensions=machine_dims, name=name)
+
+print(f'los.dell = {los.dell}')
+print(f'los.x2 = {los.x2}')
+print(f'los.x = {los.x}')
+print(f'los.y = {los.y}')
+print(f'los.z = {los.z}')
+print(f'los.r = {los.r}')
+
+# centre column
+th = np.linspace(0.0, 2*np.pi, 1000)
+x_cc = machine_dims[0][0] * np.cos(th)
+y_cc = machine_dims[0][0] * np.sin(th)
+
+# IVC
+x_ivc = machine_dims[0][1] * np.cos(th)
+y_ivc = machine_dims[0][1] * np.sin(th)
+
+plt.figure()
+plt.plot(x_cc, y_cc, 'k--')
+plt.plot(x_ivc, y_ivc, 'k--')
+plt.plot(los.x_start, los.y_start, 'ro', label='start')
+plt.plot(los.x_end, los.y_end, 'bo', label='end')
+plt.plot(los.x, los.y, 'g', label='los')
+plt.legend()
+plt.xlabel('x (m)')
+plt.ylabel('y (m)')
+plt.show()
