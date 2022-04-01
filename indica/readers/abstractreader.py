fb7e5abfab98787311b03e7f15e2d7109a8ee834
Marco Sertoli <marco.sertoli@outlook.com>
2022-04-01 20:59:25 +0100

    Solved minor conflicts with original master version

--- indica/readers/abstractreader.py
+++ indica/readers/abstractreader.py
@@ -2007,12 +2007,12 @@ class DataReader(BaseIO):
             if cached_vals.ndim == 0:
                 cached_vals = np.array([cached_vals])
         else:
-            cached_vals = intrinsic_bad
+            cached_vals = np.array(intrinsic_bad)
         ignored = self._selector(
             data, channel_dim, [*intrinsic_bad, *bad_channels], cached_vals
         )
         form = "%d" if np.issubdtype(dtype, np.integer) else "%.18e"
-        np.savetxt(cache_file, ignored, form)
+        np.savetxt(cache_file, np.array(ignored), form)
         return ignored
 
     def _set_times_item(
