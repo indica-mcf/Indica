2419b92576899af8a73a275396e8ab1d30165ed6
Marco Sertoli <marco.sertoli@outlook.com>
2022-04-05 00:34:12 +0100

    First set of reader tests completed using scans over random values in desired range

--- tests/unit/readers/test_abstract_reader_pytest.py
+++ tests/unit/readers/test_abstract_reader_pytest.py
@@ -1,192 +1,297 @@
 """Test methods present on the base class DataReader."""
 
 from copy import deepcopy
-import datetime
-from numbers import Number
-import os
-from typing import Any
-from typing import Collection
-from typing import Dict
-from typing import Hashable
-from typing import Iterable
-from typing import List
-from typing import Set
-from typing import Tuple
-
 import numpy as np
-import prov.model as prov
+import xarray as xr
 from xarray import DataArray
-
-from .available_quantities import AVAILABLE_QUANTITIES
-from .selectors import choose_on_plot
-from .selectors import DataSelector
-from ..abstractio import BaseIO
-from ..converters import FluxSurfaceCoordinates
-from ..converters import LinesOfSightTransform
-from ..converters import MagneticCoordinates
-from ..converters import TransectCoordinates
-from ..converters import TrivialTransform
-from ..datatypes import ArrayType
-from ..numpy_typing import ArrayLike
-from ..numpy_typing import RevisionLike
-from ..session import hash_vals
-from ..session import Session
-from ..utilities import to_filename
-
-from collections import defaultdict
-from contextlib import contextmanager
-from copy import copy
-import datetime
-import os
-from tempfile import TemporaryDirectory
-from time import sleep
-from unittest.mock import MagicMock
-from unittest.mock import patch
-
-from hypothesis import given
-from hypothesis import settings
-from hypothesis.strategies import composite
-from hypothesis.strategies import dictionaries
-from hypothesis.strategies import floats
-from hypothesis.strategies import from_regex
-from hypothesis.strategies import integers
-from hypothesis.strategies import just
-from hypothesis.strategies import lists
-from hypothesis.strategies import none
-from hypothesis.strategies import one_of
-from hypothesis.strategies import sampled_from
-from hypothesis.strategies import text
-from hypothesis.strategies import tuples
-import numpy as np
-import prov.model as prov
 from pytest import approx
-from pytest import mark
-from xarray import DataArray
-from xarray import ones_like
+from numbers import Number
+from typing import Set
+from typing import Collection
 
-from indica.converters import LinesOfSightTransform
-from indica.converters import MagneticCoordinates
-from indica.converters import TransectCoordinates
-from indica.datatypes import SPECIFIC_ELEMENTS
-from indica.utilities import coord_array
-from .mock_reader import ConcreteReader
-from .mock_reader import MockReader
-from ..converters.test_lines_of_sight import los_coordinates_and_axes
-from ..converters.test_magnetic import magnetic_coordinates_and_axes
-from ..converters.test_transect import transect_coordinates_and_axes
-from ..data_strategies import array_dictionaries
-from ..data_strategies import data_arrays
-from ..data_strategies import equilibrium_data
-from ..data_strategies import general_datatypes
-from ..data_strategies import specific_datatypes
-from ..strategies import machine_dimensions
-from ..strategies import sane_floats
+from indica.readers import PPFReader
+from indica.numpy_typing import RevisionLike
+from indica.readers.available_quantities import AVAILABLE_QUANTITIES
 
 
-nsamples = 10
-nts = np.random.randint(10, 50, nsamples)
-dts = np.random.uniform(0.001, 1., nsamples)
-tstarts = np.random.random(nsamples)
-tends = tstarts + dts * nts
-revisions = np.random.randint(0, 10, nsamples)
-lengths = np.random.randint(0, 20, nsamples)
-
-Rmin, Rmax = 0.2, 3.
-zmin, zmax = -1, 1
-
-def generate_data(size, low, high):
-    return np.random.uniform(low=low, high=high, size=size)
+# TODO these values should come from the machine dimensions variable of the reader
 
 
-def expected_data(
-    diagnostic:str,
-    empty=False,
+class Generate_data:
+    def __init__(self):
+        self.tstart = 0
+        self.tend = 10
+
+        self.length = np.random.randint(4, 20)
+        self.dt = np.random.uniform(0.001, 1.0)
+        self.times = np.arange(self.tstart, self.tend, self.dt)
+        self.texp = np.full_like(self.times, self.dt)
+        nt = len(self.times)
+
+        self.element = "element"
+
+        self.machine_dims = ((1.83, 3.9), (-1.75, 2.0))
+        self.revision = np.random.randint(0, 10)
+        self.times = self.times
+        self.texp = self.texp
+
+        self.R = np.random.uniform(0.2, 3.0, (self.length,))
+        self.z = np.random.uniform(-1, 1, (self.length,))
+        self.z_ece = np.random.uniform(-1, 1)
+
+        self.Te = np.random.uniform(10, 10.0e3, (nt, self.length))
+        self.Te_error = np.sqrt(self.Te)
+        self.Ti = deepcopy(self.Te)
+        self.Ti_error = deepcopy(self.Te_error)
+        self.Ne = np.random.uniform(1.0e16, 1.0e21, (nt, self.length))
+        self.Ne_error = np.sqrt(self.Ne)
+        self.angf = np.random.uniform(1.0e2, 1.0e6, (nt, self.length))
+        self.angf_error = np.sqrt(self.angf)
+        self.conc = np.random.uniform(1.e-6, 1.e-1, (nt, self.length))
+        self.conc_error = np.sqrt(self.conc)
+
+        self.Btot = np.random.uniform(0.1, 5, (self.length))
+        self.bad_channels = []
+
+def _select_channels(
+    category: str,
+    uid: str,
+    instrument: str,
+    quantity: str,
+    data: DataArray,
+    channel_dim: str,
+    bad_channels: Collection[Number] = [],
 ):
-    """Strategy to produce a dictionary of DataArrays of the type that
-    could be returned by a read operation.
-    """
+    return []
 
-    if empty:
-        return {}
-
-    isample = np.random.randint(0, 10)
-    tstart = tstarts[isample]
-    tend = tends[isample]
-    dt = dts[isample]
-    revision = revisions[isample]
-    length = lengths[isample]
-
-    low = 100.
-    high = 5.e3
-
-    diagnostic_coord = diagnostic + "_coord"
-
-    results = {}
-    results["revision"] = revision
-    results["length"] = length
-    results["R"] = np.sort(np.random.random(length) * Rmax + Rmin)
-    results["z"] = np.sort(np.random.random(2) * zmax + zmin)
-    results["te"] = generate_data()
-    time = coord_array(np.arange(tstart, tend+dt, dt), "t")
-
-    data =
-
-    for array in result.values():
-        transform = array.attrs["transform"]
-        if hasattr(transform, "equilibrium"):
-            del transform.equilibrium
-        if isinstance(transform, TransectCoordinates):
-            x1name = transform.x1_name
-            to_fix = [array]
-            if "error" in array.attrs:
-                to_fix.append(array.attrs["error"])
-            if "dropped" in array.attrs:
-                to_fix.append(array.attrs["dropped"])
-                if "error" in array.attrs:
-                    to_fix.append(array.attrs["dropped"].attrs["error"])
-            for a in to_fix:
-                a.coords["R"] = DataArray(
-                    transform.R_vals.y, coords=[(x1name, a.coords[x1name].values)]
-                )
-                a.coords["z"] = DataArray(
-                    transform.z_vals.y, coords=[(x1name, a.coords[x1name].values)]
-                )
-        if isinstance(transform, MagneticCoordinates):
-            to_fix = [array]
-            if "error" in array.attrs:
-                to_fix.append(array.attrs["error"])
-            if "dropped" in array.attrs:
-                to_fix.append(array.attrs["dropped"])
-                if "error" in array.attrs:
-                    to_fix.append(array.attrs["dropped"].attrs["error"])
-            for a in to_fix:
-                a.coords["z"] = transform.z_los
-    return result
-
-def test_thomson_scattering(data_instrument, uid, revision, time_range, max_freq):
+def test_get_thomson_scattering(nsamples=10):
     """Test the get_thomson_scattering method correctly combines and processes
     raw data."""
-    data, instrument = data_instrument
-    for quantity, array in data.items():
-        array.name = instrument + "_" + quantity
-    reader = MockReader(True, True, *time_range, max_freq)
-    reader.set_thomson_scattering(next(iter(data.values())), data)
-    quantities = set(data)
-    results = reader.get_thomson_scattering(uid, instrument, revision, quantities)
-    reader._get_thomson_scattering.assert_called_once_with(
-        uid, instrument, revision, quantities
-    )
-    for q, actual, expected in [(q, results[q], data[q]) for q in quantities]:
-        assert_data_arrays_equal(actual, expected, *time_range, max_freq)
-        assert_any_call(
-            reader.create_provenance,
-            "thomson_scattering",
-            uid,
-            instrument,
-            revision,
-            q,
-            [],
-            find_dropped_channels(expected, expected.dims[1]),
-        )
+
+    def _get_thomson_scattering(
+            uid: str,
+            instrument: str,
+            revision: RevisionLike,
+            quantities: Set[str],
+            empty=False,
+    ):
+        """Strategy to produce a dictionary of DataArrays of the type that
+        could be returned by a read operation.
+        """
+
+        if empty:
+            return {}
+
+        database_results: dict = {}
+        database_results["times"] = data.times
+        database_results["revision"] = data.revision
+        database_results["length"] = data.length
+        database_results["z"] = data.z
+        database_results["R"] = data.R
+        database_results["te"] = data.Te
+        database_results["ne"] = data.Ne
+        database_results["te_error"] = data.Te_error
+        database_results["ne_error"] = data.Ne_error
+
+        for quantity in quantities:
+            database_results[f"{quantity}_records"] = [
+                f"{quantity}_Rz_path",
+                f"{quantity}_value_path",
+                f"{quantity}_error_path",
+            ]
+
+        return database_results
+
+    for i in range(nsamples):
+        data = Generate_data()
+
+        category = "get_thomson_scattering"
+        uid = "jetppf"
+        instruments = ["hrts", "lidr"]
+        quantities = AVAILABLE_QUANTITIES[category]
+
+        reader = PPFReader(0, data.tstart, data.tend + data.dt)
+        reader._get_thomson_scattering = _get_thomson_scattering
+        reader._select_channels = _select_channels
+
+        for instrument in instruments:
+            database_results = reader._get_thomson_scattering(uid, instrument, data.revision, quantities)
+            results = reader.get_thomson_scattering(uid, instrument, data.revision, quantities)
+
+            for q, actual, expected in [(q, results[q], database_results[q]) for q in quantities]:
+                assert np.all(actual.values == expected)
+
+def test_get_charge_exchange(nsamples=10):
+    """Test the get_charge_exchange method correctly combines and processes
+    raw data."""
+
+    def _get_charge_exchange(
+        uid: str,
+        instrument: str,
+        revision: RevisionLike,
+        quantities: Set[str],
+        empty=False,
+    ):
+        """Strategy to produce a dictionary of DataArrays of the type that
+        could be returned by a read operation.
+        """
+
+        if empty:
+            return {}
+
+        database_results: dict = {}
+        database_results["R"] = data.R
+        database_results["z"] = data.z
+        database_results["length"] = data.length
+        database_results["element"] = data.element
+        database_results["texp"] = data.texp
+        database_results["times"] = data.times
+        database_results["angf"] = data.angf
+        database_results["angf_error"] = data.angf_error
+        database_results["conc"] = data.conc
+        database_results["conc_error"] = data.conc_error
+        database_results["ti"] = data.Ti
+        database_results["ti_error"] = data.Ti_error
+        database_results["revision"] = data.revision
+
+        for quantity in quantities:
+            database_results[f"{quantity}_records"] = [
+                f"{quantity}_R_path",
+                f"{quantity}_z_path",
+                f"{quantity}_element_path",
+                f"{quantity}_time_path",
+                f"{quantity}_value_path",
+                f"{quantity}_error_path",
+            ]
+
+        return database_results
+
+    for i in range(nsamples):
+        data = Generate_data()
+
+        category = "get_charge_exchange"
+        uid = "jetppf"
+        instruments = ["cxg6"]
+        quantities = AVAILABLE_QUANTITIES[category]
+
+        reader = PPFReader(0, data.tstart, data.tend + data.dt)
+        reader._get_charge_exchange = _get_charge_exchange
+        reader._select_channels = _select_channels
+
+        for instrument in instruments:
+            database_results = reader._get_charge_exchange(uid, instrument, data.revision, quantities)
+            results = reader.get_charge_exchange(uid, instrument, data.revision, quantities)
+
+            for q, actual, expected in [(q, results[q], database_results[q]) for q in quantities]:
+                assert np.all(actual.values == expected)
 
 
+def test_cyclotron_emissions(nsamples=10):
+    """Test the get_charge_exchange method correctly combines and processes
+    raw data."""
+
+    def _get_cyclotron_emissions(
+        uid: str,
+        instrument: str,
+        revision: RevisionLike,
+        quantities: Set[str],
+        empty=False,
+    ):
+        """Strategy to produce a dictionary of DataArrays of the type that
+        could be returned by a read operation.
+        """
+
+        if empty:
+            return {}
+
+        database_results: dict = {}
+        database_results["machine_dims"] = data.machine_dims
+        database_results["z"] = data.z_ece
+        database_results["length"] = data.length
+        database_results["Btot"] = data.Btot
+        database_results["bad_channels"] = data.bad_channels
+        database_results["times"] = data.times
+        database_results["te"] = data.Te
+        database_results["te_error"] = data.Te_error
+        database_results["revision"] = data.revision
+
+        for quantity in quantities:
+            database_results[f"{quantity}_records"] = []
+            for i in range(data.length):
+                database_results[f"{quantity}_records"].append(f"chan_{i}_path")
+
+        return database_results
+
+    for i in range(nsamples):
+        data = Generate_data()
+
+        category = "get_cyclotron_emissions"
+        uid = "jetppf"
+        instruments = ["kk3"]
+        quantities = AVAILABLE_QUANTITIES[category]
+
+        reader = PPFReader(0, data.tstart, data.tend + data.dt)
+        reader._get_cyclotron_emissions = _get_cyclotron_emissions
+        reader._select_channels = _select_channels
+
+        for instrument in instruments:
+            database_results = reader._get_cyclotron_emissions(uid, instrument, data.revision, quantities)
+            results = reader.get_cyclotron_emissions(uid, instrument, data.revision, quantities)
+
+            for q, actual, expected in [(q, results[q], database_results[q]) for q in quantities]:
+                assert np.all(actual.values == expected)
+#
+# def test_get_radiation(nsamples=10):
+#     """Test the get_charge_exchange method correctly combines and processes
+#     raw data."""
+#
+#     def _get_radiation(
+#         uid: str,
+#         instrument: str,
+#         revision: RevisionLike,
+#         quantities: Set[str],
+#         empty=False,
+#     ):
+#         """Strategy to produce a dictionary of DataArrays of the type that
+#         could be returned by a read operation.
+#         """
+#
+#         if empty:
+#             return {}
+#
+#         database_results: dict = {}
+#         database_results["machine_dims"] = data.machine_dims
+#         database_results["z"] = data.z_ece
+#         database_results["length"] = data.length
+#         database_results["Btot"] = data.Btot
+#         database_results["bad_channels"] = data.bad_channels
+#         database_results["times"] = data.times
+#         database_results["te"] = data.Te
+#         database_results["te_error"] = data.Te_error
+#         database_results["revision"] = data.revision
+#
+#         for quantity in quantities:
+#             database_results[f"{quantity}_records"] = []
+#             for i in range(data.length):
+#                 database_results[f"{quantity}_records"].append(f"chan_{i}_path")
+#
+#         return database_results
+#
+#     for i in range(nsamples):
+#         data = Generate_data()
+#
+#         category = "get_cyclotron_emissions"
+#         uid = "jetppf"
+#         instruments = ["kk3"]
+#         quantities = AVAILABLE_QUANTITIES[category]
+#
+#         reader = PPFReader(0, data.tstart, data.tend + data.dt)
+#         reader._get_radiation = _get_radiation
+#         reader._select_channels = _select_channels
+#
+#         for instrument in instruments:
+#             database_results = reader._get_radiation(uid, instrument, data.revision, quantities)
+#             results = reader.get_radiation(uid, instrument, data.revision, quantities)
+#
+#             for q, actual, expected in [(q, results[q], database_results[q]) for q in quantities]:
+#                 assert np.all(actual.values == expected)
