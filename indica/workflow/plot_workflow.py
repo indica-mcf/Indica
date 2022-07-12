46d6382193879ddf3d7a54e0d15d7e37a983ba09
Kingsley Collie <kingsley.collie@ukaea.uk>
2022-07-12 17:34:35 +0100

    Add plot methods:
    - Back calculated SXR
    - Emissivity on midplane

--- indica/workflow/plot_workflow.py
+++ indica/workflow/plot_workflow.py
@@ -2,8 +2,10 @@ from typing import Union
 
 from matplotlib import pyplot as plt
 import numpy as np
+import xarray as xr
 from xarray import DataArray
 
+from indica.operators.extrapolate_impurity_density import recover_threshold_rho
 from .base_workflow import BaseWorkflow
 
 
@@ -42,7 +44,7 @@ class PlotWorkflow:
         ).sel({"t": time}, method="nearest").plot(x="R", ax=ax, *args, **kwargs)
         return fig, ax
 
-    def plot_midplane(
+    def plot_density_midplane(
         self,
         threshold_rho: float = None,
         time: Union[int, float] = None,
@@ -89,4 +91,77 @@ class PlotWorkflow:
             ax=ax, label="Derived bolometry", *args, **kwargs
         )
         ax.legend()
+        return fig, ax
+
+    def plot_sxr_los_fit(self, time):
+        fig, ax = plt.subplots()
+        self.workflow.additional_data["invert_sxr"]["camera_results"][0].camera.sel(
+            t=time, method="nearest"
+        ).plot(ax=ax, x="sxr_v_rho_poloidal", label="camera")
+        self.workflow.additional_data["invert_sxr"]["camera_results"][
+            0
+        ].back_integral.sel(t=time, method="nearest").plot(
+            ax=ax, x="sxr_v_rho_poloidal", label="back_integral"
+        )
+        ax.legend()
+        return fig, ax
+
+    def plot_emissivity_midplane_R(
+        self,
+        threshold_rho: float = None,
+        time: Union[int, float] = None,
+        *args,
+        **kwargs
+    ):
+        if time is None:
+            time = self.default_time
+        fig, ax = plt.subplots()
+        self.workflow.sxr_emissivity.interp(
+            {"rho_poloidal": self.rho_deriv, "theta": self.theta_deriv}
+        ).sel(
+            {"t": time, "z": self.zmag.sel({"t": time}, method="nearest")},
+            method="nearest",
+        ).plot.line(
+            x="R", ax=ax, *args, **kwargs
+        )
+        if threshold_rho is not None:
+            threshold_R_outer, _ = self.workflow.sxr_emissivity.attrs[
+                "transform"
+            ].convert_to_Rz(threshold_rho, 0, self.workflow.t)
+            threshold_R_inner, _ = self.workflow.sxr_emissivity.attrs[
+                "transform"
+            ].convert_to_Rz(threshold_rho, np.pi, self.workflow.t)
+            ax.axvline(threshold_R_outer.sel({"t": time}, method="nearest"))
+            ax.axvline(threshold_R_inner.sel({"t": time}, method="nearest"))
+        return fig, ax
+
+    def plot_emissivity_midplane_rho(
+        self, time: Union[int, float] = None, *args, **kwargs
+    ):
+        if time is None:
+            time = self.default_time
+        fig, ax = plt.subplots()
+
+        emiss_lfs = (
+            self.workflow.sxr_emissivity.sel(theta=0, method="nearest")
+            .copy()
+            .transpose()
+        )
+        emiss_hfs = self.workflow.sxr_emissivity.sel(
+            theta=np.pi, method="nearest"
+        ).copy()
+        emiss_hfs = emiss_hfs.assign_coords(
+            {"rho_poloidal": -emiss_hfs.rho_poloidal}
+        ).transpose()
+
+        emiss_all = xr.concat([emiss_hfs[::-1], emiss_lfs], dim="rho_poloidal")
+
+        emiss_all.sel(t=time, method="nearest").plot(ax=ax)
+
+        threshold_rho = recover_threshold_rho(
+            1.5e3, self.workflow.electron_temperature
+        ).sel(t=time, method="nearest")
+        ax.axvline(threshold_rho)
+        ax.axvline(-threshold_rho)
+
         return fig, ax
