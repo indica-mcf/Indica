fac9be84770ef2c316e5e3830ef55065659daf13
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2022-08-03 10:48:55 +0100

    Completed new data massaging methods (time binning and remapping to equilibrium) in agreement with the new line of sight transform methods

--- hda/manage_data.py
+++ hda/manage_data.py
@@ -3,8 +3,12 @@ import xarray as xr
 from xarray import DataArray
 from hda.utils import print_like
 from hda.models.plasma import Plasma
-from indica.converters.time import bin_in_time_dt
+from indica.converters.time import bin_in_time_dt, get_tlabels_dt
+from indica.equilibrium import Equilibrium
+from indica.converters import FluxSurfaceCoordinates
+from indica.converters import lines_of_sight, line_of_sight
 from copy import deepcopy
+
 
 def initialize_bckc(diagnostic, quantity, data, bckc={}):
     """
@@ -23,111 +27,101 @@ def initialize_bckc(diagnostic, quantity, data, bckc={}):
     if diagnostic not in bckc:
         bckc[diagnostic] = {}
 
-    data_tmp = data[diagnostic][quantity]
-    bckc_tmp = xr.full_like(data_tmp, np.nan)
-    attrs = bckc_tmp.attrs
-    if type(bckc_tmp) == DataArray:
-        if "error" in attrs.keys():
-            attrs["error"] = xr.full_like(attrs["error"], np.nan)
-        if "partial_provenance" in attrs.keys():
-            attrs.pop("partial_provenance")
-            attrs.pop("provenance")
-    bckc_tmp.attrs = attrs
-
+    bckc_tmp = initialize_bckc_quantity(data[diagnostic], quantity)
     bckc[diagnostic][quantity] = bckc_tmp
 
     return bckc
 
 
-def remap_diagnostic(diag_data, flux_transform, npts=100):
-    """
-    Calculate maping on equilibrium for speccified diagnostic
+def initialize_bckc_quantity(diagnostic_data: dict, quantity):
+    data = diagnostic_data[quantity]
+    bckc = xr.full_like(data, np.nan)
+    attrs = bckc.attrs
+    if type(bckc) == DataArray:
+        if "error" in attrs.keys():
+            attrs["error"] = xr.full_like(attrs["error"], np.nan)
+        if "partial_provenance" in attrs.keys():
+            attrs.pop("partial_provenance")
+            attrs.pop("provenance")
+    bckc.attrs = attrs
 
-    Returns
-    -------
+    return bckc
 
-    """
-    new_attrs = {}
-    trans = diag_data.attrs["transform"]
-    x1 = diag_data.coords[trans.x1_name]
-    x2_arr = np.linspace(0, 1, npts)
-    x2 = DataArray(x2_arr, dims=trans.x2_name)
-    dl = trans.distance(trans.x2_name, DataArray(0), x2[0:2], 0)[1]
-    new_attrs["x2"] = x2
-    new_attrs["dl"] = dl
-    new_attrs["R"], new_attrs["z"] = trans.convert_to_Rz(x1, x2, 0)
 
-    dt_equil = flux_transform.equilibrium.rho.t[1] - flux_transform.equilibrium.rho.t[0]
-    dt_data = diag_data.t[1] - diag_data.t[0]
-    if dt_data > dt_equil:
-        t = diag_data.t
-    else:
-        t = None
-    rho_equil, _ = flux_transform.convert_from_Rz(new_attrs["R"], new_attrs["z"], t=t)
-    rho = rho_equil.interp(t=diag_data.t, method="linear")
-    rho = xr.where(rho >= 0, rho, 0.0)
-    rho.coords[trans.x2_name] = x2
-    new_attrs["rho"] = rho
-
-    return new_attrs
-
-def build_data(plasma: Plasma, data, instrument=""):
+def bin_data_in_time(
+    exp_data: dict,
+    tstart: float,
+    tend: float,
+    dt: float,):
     """
     Bin raw experimental data on the desired time axis, assign equilibrium to
     transform objects
 
     Parameters
     ----------
-    plasma
-        Plasma class
-    data
-        Experimental data dictionary whose elements are the data structures
-        returned by Indica's abstractreader, its keys = instrument identifiers
-    instrument
-        Build data only for specified instrument
+    raw_data
+        Experimental data as returned by Indica's abstractreader.py
+    tstart
+        Start of time range for which to get data.
+    tend
+        End of time range for which to get data.
+    dt
+        Time binning/interpolation
 
     Returns
     -------
-
+    Data dictionary identical to input value but binned on new time axis
     """
-    print_like("Building data class")
     binned_data = {}
+    for quant in exp_data.keys():
+        data = deepcopy(exp_data[quant])
+        attrs = data.attrs
+        if "t" in data.coords:
+            data = bin_in_time_dt(tstart, tend, dt, data)
+        if "provenance" in attrs:
+            data.attrs["provenance"] = attrs["provenance"]
 
-    for kinstr in data.keys():
-        if (len(instrument) > 0) and (kinstr != instrument):
-            continue
-        instrument_data = {}
-
-        if type(data[kinstr]) != dict:
-            value = deepcopy(data[kinstr])
-            if np.size(value) > 1:
-                value = bin_in_time_dt(plasma.tstart, plasma.tend, plasma.dt, value)
-            binned_data[kinstr] = value
-            continue
-
-        for kquant in data[kinstr].keys():
-            value = data[kinstr][kquant]
-            if "t" in value.coords:
-                value = bin_in_time_dt(plasma.tstart, plasma.tend, plasma.dt, value)
-
-            if "transform" in data[kinstr][kquant].attrs and hasattr(plasma, "equilibrium"):
-                value.attrs["transform"] = data[kinstr][kquant].transform
-                value.transform.set_equilibrium(plasma.equilibrium, force=True)
-                if "LinesOfSightTransform" in str(value.attrs["transform"]):
-                    geom_attrs = remap_diagnostic(value, plasma.flux_coords)
-                    for kattrs in geom_attrs:
-                        value.attrs[kattrs] = geom_attrs[kattrs]
-
-            if "provenance" in data[kinstr][kquant].attrs:
-                value.attrs["provenance"] = data[kinstr][kquant].provenance
-
-            instrument_data[kquant] = value
-
-        binned_data[kinstr] = instrument_data
-        if kinstr == instrument:
-            break
+        binned_data[quant] = data
 
     return binned_data
+
+
+def map_on_equilibrium(
+    diagnostic_data: dict,
+    flux_transform: FluxSurfaceCoordinates,
+):
+    """
+    Assign equilibrium and transform, map viewing LOS
+
+    Parameters
+    ----------
+    diagnostic_data
+        Experimental data of a specific instrument as returned by Indica's abstractreader.py
+    flux_transform
+        Indica's FluxSurfaceTransform object
+
+    Returns
+    -------
+    Data dictionary identical to input with transforms set for remapping
+    and remapping
+    """
+
+    data = diagnostic_data[list(diagnostic_data)[0]]
+    if "transform" not in data.attrs:
+        return data
+
+    transform = data.attrs["transform"]
+    if hasattr(flux_transform, "equilibrium"):
+        transform.set_equilibrium(flux_transform.equilibrium, force=True)
+        if "LinesOfSightTransform" in str(data.attrs["transform"]):
+            transform.set_flux_transform(flux_transform)
+            transform.remap_los(t=data.t)
+
+    for quantity in diagnostic_data.keys():
+        diagnostic_data[quantity].attrs["transform"] = transform
+
+    return diagnostic_data
+
 
 def apply_limits(
     data,
