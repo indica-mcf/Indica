133eacf2b543f6b84fca67e9827cb47e593870c2
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2022-08-03 17:13:50 +0100

    Moved los-integral calculation to line of sight transform

--- hda/utils.py
+++ hda/utils.py
@@ -1,23 +1,5 @@
 from xarray import DataArray
 from copy import deepcopy
-import xarray as xr
-
-def calc_los_int(experimental_data, profile_to_integrate, t=None):
-    dl = experimental_data.attrs["dl"]
-    rho = experimental_data.attrs["rho"]
-    transform = experimental_data.attrs["transform"]
-
-    x2_name = transform.x2_name
-
-    value = profile_to_integrate.interp(rho_poloidal=rho)
-    if t is not None:
-        value = value.sel(t=t)
-        rho = rho.sel(t=t)
-    value = xr.where(rho <= 1, value, 0, )
-    los_integral = value.sum(x2_name) * dl
-
-    return los_integral
-
 
 def assign_datatype(data_array: DataArray, datatype: tuple, unit=""):
     data_array.name = f"{datatype[1]}_{datatype[0]}"
