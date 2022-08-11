218d962d7b0e5498aed091ff7ac1d2e46afb4249
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2022-08-11 16:25:59 +0100

    completed transition to independent model and optimisation class/methods

--- hda/models/interferometer.py
+++ hda/models/interferometer.py
@@ -1,4 +1,5 @@
-from indica.converters import lines_of_sight, line_of_sight
+from indica.converters.lines_of_sight import LinesOfSightTransform
+from indica.converters.line_of_sight import LineOfSightTransform
 from indica.converters import FluxSurfaceCoordinates
 from indica.numpy_typing import LabeledArray
 
@@ -11,7 +12,7 @@ class Interferometer:
     def __init__(self, name: str):
         self.name = name
 
-    def set_los_transform(self, transform: lines_of_sight, passes:int=2):
+    def set_los_transform(self, transform: LinesOfSightTransform, passes:int=2):
         """
         Parameters
         ----------
@@ -29,22 +30,9 @@ class Interferometer:
         """
         self.los_transform.set_flux_transform(flux_transform)
 
-    def map_los(self, t: LabeledArray = None, npts=100):
+    def map_to_los(self, Ne: LabeledArray, t: LabeledArray = None):
         """
-        Map line of sight on equilibrium reconstruction
-
-        Parameters
-        ----------
-        t
-            time (s)
-        npts
-            number of points along the line of sight
-        """
-        self.los_transform.remap_los(t=t, npts=npts)
-
-    def line_integrated_density(self, Ne: LabeledArray, t: LabeledArray = None):
-        """
-        Calculate the integral of the electron density along the line of sight
+        Map interferometer measurements along line of sight
 
         Parameters
         ----------
@@ -58,7 +46,30 @@ class Interferometer:
         Return line integral and interpolated density along the line of sight
 
         """
-        los_integral, along_los = self.los_transform.integrate_on_los(Ne, t=t, passes=self.passes)
+        along_los = {}
+        along_los["ne"] = self.los_transform.map_to_los(Ne, t=t)
+        return along_los
+
+    def integrate_on_los(self, Ne: LabeledArray, t: LabeledArray = None):
+        """
+        Calculate the integral of the interferometer measurement along the line of sight
+
+        Parameters
+        ----------
+        Ne
+            1D profile of the electron density
+        t
+            time (s)
+
+        Returns
+        -------
+        Return line integral and interpolated density along the line of sight
+
+        """
+        along_los = {}
+        los_integral = {}
+        los_integral["ne"], along_los["ne"] = self.los_transform.integrate_on_los(Ne, t=t, passes=self.passes)
+
         return los_integral, along_los
 
     def line_integrated_phase_shift(self, Ne: LabeledArray, t: LabeledArray = None):
