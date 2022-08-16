31b39c3c4dcf1d5330312bc1fe542cce73b4c1af
Kingsley Collie <kingsley.collie@ukaea.uk>
2022-08-16 10:29:54 +0100

    Add mean charge to toroidal rotation

--- indica/operators/centrifugal_asymmetry.py
+++ indica/operators/centrifugal_asymmetry.py
@@ -49,6 +49,7 @@ class ToroidalRotation(Operator):
         impurity: str,
         Zeff: DataArray,
         electron_temp: DataArray,
+        mean_charge: DataArray,
     ):
         """Calculates the toroidal rotation frequency from the asymmetry parameter.
 
@@ -66,6 +67,8 @@ class ToroidalRotation(Operator):
             xarray.DataArray containing Z-effective data from diagnostics.
         electron_temp
             xarray.DataArray containing electron temperature data. In units of eV.
+        mean_charge
+            xarray.DataArray containing impurity mean charge data
 
         Returns
         -------
@@ -114,6 +117,14 @@ class ToroidalRotation(Operator):
             strictly_positive=True,
         )
 
+        input_check(
+            "mean_charge",
+            mean_charge,
+            DataArray,
+            ndim_to_check=3,
+            greater_than_or_equal_zero=False,
+        )
+
         asymmetry_parameter = asymmetry_parameters.sel(element=impurity)
 
         impurity_mass_int = ELEMENTS[impurity][1]
@@ -121,7 +132,7 @@ class ToroidalRotation(Operator):
         unified_atomic_mass_unit = 931.4941e6  # in eV/c^2
         impurity_mass = float(impurity_mass_int) * unified_atomic_mass_unit
 
-        mean_charge = ELEMENTS[impurity][0]
+        mean_charge = mean_charge.sel({"element": impurity}, drop=True)
 
         main_ion_mass_int = ELEMENTS[main_ion][1]
 
