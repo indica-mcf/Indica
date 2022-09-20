458529579fbe66f9a628fec2e3d6b4a8fa835471
Kingsley Collie <kingsley.collie@ukaea.uk>
2022-09-20 15:30:35 +0100

    Refactor to use equilibrium

--- indica/asymmetric_quantity.py
+++ indica/asymmetric_quantity.py
@@ -1,6 +1,66 @@
 import numpy as np
 import xarray as xr
 
+from indica.converters import FluxSurfaceCoordinates
+from indica.equilibrium import Equilibrium
+
+
+def check_grids(a: xr.DataArray, b: xr.DataArray):
+    if a.rho_poloidal != b.rho_poloidal:
+        raise ValueError("Values must be on the same rho grid.")
+
+
+def check_equilibria(a: Equilibrium, b: Equilibrium):
+    if a != b:
+        raise ValueError("Equilibria must be the same.")
+
+
+def calc_R_square_diff(
+    rho_grid: xr.DataArray, time_grid: xr.DataArray, equilibrium: Equilibrium
+):
+    flux_coords = FluxSurfaceCoordinates(kind="poloidal")
+    flux_coords.set_equilibrium(equilibrium)
+    R_lfs, _ = flux_coords.convert_to_Rz(
+        rho_grid,
+        xr.zeros_like(rho_grid),
+        time_grid,
+    )
+    R_hfs, _ = flux_coords.convert_to_Rz(
+        rho_grid,
+        np.pi * xr.ones_like(rho_grid),
+        time_grid,
+    )
+
+    return R_hfs**2 - R_lfs**2
+
+
+def asymmetry_parameter_from_modifier(
+    asymmetry_modifier: xr.DataArray, equilibrium: Equilibrium
+):
+    R_square_diff = calc_R_square_diff(
+        asymmetry_modifier.rho_poloidal,
+        asymmetry_modifier.t,
+        equilibrium,
+    )
+
+    return np.log(asymmetry_modifier) / R_square_diff
+
+
+def asymmetry_parameter_from_lfs_hfs(
+    lfs_values: xr.DataArray,
+    hfs_values: xr.DataArray,
+    equilibrium: Equilibrium,
+):
+    return asymmetry_parameter_from_modifier(
+        asymmetry_modifier_from_lfs_hfs(lfs_values, hfs_values),
+        equilibrium,
+    )
+
+
+def asymmetry_modifier_from_lfs_hfs(lfs_values: xr.DataArray, hfs_values: xr.DataArray):
+    check_grids(hfs_values, lfs_values)
+    return hfs_values / lfs_values
+
 
 class AsymmetricQuantity:
     """
@@ -10,11 +70,11 @@ class AsymmetricQuantity:
     Parameters
     ----------
     lfs_values: xr.DataArray
-        The values of n_s(rho, R_0; t)
+        The (low field side) values of n_s(rho, R_0; t) on coordinates (rho_poloidal, t)
     asymmetry_parameter: xr.DataArray
-        The values of lambda_s(rho; t)
-    R_square_diff: xr.DataArray
-        The values of R(rho; t)^2 - R_0(rho; t)^2
+        The values of lambda_s(rho; t) on coordinates (rho_poloidal, t)
+    equilibrium: Equilibrium
+        The plasma equilibrium
 
     """
 
@@ -22,55 +82,58 @@ class AsymmetricQuantity:
         self,
         lfs_values: xr.DataArray,
         asymmetry_parameter: xr.DataArray,
-        R_square_diff: xr.DataArray,
+        equilibrium: Equilibrium,
     ):
         self.lfs_values = lfs_values
         self.asymmetry_parameter = asymmetry_parameter
-        self.R_square_diff = R_square_diff
+
+        check_grids(lfs_values, asymmetry_parameter)
+
+        self.equilibrium = equilibrium
 
     @classmethod
-    def from_midplane(
-        cls, rho_midplane_values: xr.DataArray, R_square_diff: xr.DataArray
-    ):
+    def from_midplane(cls, rho_midplane_values: xr.DataArray, equilibrium: Equilibrium):
         lfs_values = rho_midplane_values.sel(rho_poloidal=slice(0, None))
         hfs_values = rho_midplane_values.sel(rho_poloidal=slice(None, 0))
-        return cls.from_lfs_hfs(lfs_values, hfs_values, R_square_diff)
+
+        hfs_values = hfs_values.assign_coords(-hfs_values.rho_poloidal)
+        check_grids(lfs_values, hfs_values)
+
+        return cls.from_lfs_hfs(lfs_values, hfs_values, equilibrium)
 
     @classmethod
     def from_lfs_hfs(
         cls,
         lfs_values: xr.DataArray,
         hfs_values: xr.DataArray,
-        R_square_diff: xr.DataArray,
+        equilibrium: Equilibrium,
     ):
-        asymmetry_parameter = cls.asymmetry_parameter_from_lfs_hfs(
-            lfs_values, hfs_values, R_square_diff
+        asymmetry_parameter = asymmetry_parameter_from_lfs_hfs(
+            lfs_values, hfs_values, equilibrium
         )
-        return cls(lfs_values, asymmetry_parameter, R_square_diff)
+        return cls(lfs_values, asymmetry_parameter, equilibrium)
 
     @classmethod
     def from_lfs_asymmetry_modifier(
         cls,
         lfs_values: xr.DataArray,
         asymmetry_modifier: xr.DataArray,
-        R_square_diff: xr.DataArray,
+        equilibrium,
     ):
-        asymmetry_parameter = cls.asymmetry_parameter_from_modifier(
-            asymmetry_modifier, R_square_diff
+        asymmetry_parameter = asymmetry_parameter_from_modifier(
+            asymmetry_modifier, equilibrium
         )
-        return cls(lfs_values, asymmetry_parameter, R_square_diff)
+        return cls(lfs_values, asymmetry_parameter, equilibrium)
 
     @classmethod
     def from_R_z(cls, values_R_z: xr.DataArray):
         raise NotImplementedError
 
     @classmethod
-    def from_rho_theta(
-        cls, values_rho_theta: xr.DataArray, R_square_diff: xr.DataArray
-    ):
+    def from_rho_theta(cls, values_rho_theta: xr.DataArray, equilibrium: Equilibrium):
         lfs_values = values_rho_theta.sel(theta=0, method="nearest")
         hfs_values = values_rho_theta.sel(theta=np.pi, method="nearest")
-        return cls.from_lfs_hfs(lfs_values, hfs_values, R_square_diff)
+        return cls.from_lfs_hfs(lfs_values, hfs_values, equilibrium)
 
     @classmethod
     def from_rho_R(cls, values_rho_R: xr.DataArray):
@@ -85,51 +148,48 @@ class AsymmetricQuantity:
     def to_rho_R(self):
         raise NotImplementedError
 
-    def asymmetry_modifier(self):
-        return np.exp(self.asymmetry_parameter * self.R_square_diff)
+    def asymmetry_modifier(self, R_square_diff):
+        return np.exp(self.asymmetry_parameter * R_square_diff)
 
-    @classmethod
-    def asymmetry_parameter_from_modifier(
-        cls, asymmetry_modifier: xr.DataArray, R_square_diff: xr.DataArray
-    ):
-        return np.log(asymmetry_modifier) / R_square_diff
-
-    @classmethod
-    def asymmetry_parameter_from_lfs_hfs(
-        cls,
-        lfs_values: xr.DataArray,
-        hfs_values: xr.DataArray,
-        R_square_diff: xr.DataArray,
-    ):
-        return cls.asymmetry_parameter_from_modifier(
-            cls.asymmetry_modifier_from_lfs_hfs(lfs_values, hfs_values), R_square_diff
+    def R_square_diff(self):
+        flux_coords = FluxSurfaceCoordinates(kind="poloidal")
+        flux_coords.set_equilibrium(self.equilibrium)
+        R_lfs, _ = flux_coords.convert_to_Rz(
+            self.lfs_values.rho_poloidal,
+            xr.zeros_like(self.lfs_values.rho_poloidal),
+            self.lfs_values.t,
         )
-
-    @staticmethod
-    def asymmetry_modifier_from_lfs_hfs(
-        lfs_values: xr.DataArray, hfs_values: xr.DataArray
-    ):
-        return hfs_values / lfs_values
+        R_hfs, _ = flux_coords.convert_to_Rz(
+            self.lfs_values.rho_poloidal,
+            np.pi * xr.ones_like(self.lfs_values.rho_poloidal),
+            self.lfs_values.t,
+        )
+        return R_hfs**2 - R_lfs**2
 
     # TODO: check values on same rho grids or interpolate
     def __eq__(self, other) -> bool:
+        check_grids(self.lfs_values, other.lfs_values)
+        check_equilibria(self.equilibrium, other.equilibrium)
         return (
             self.lfs_values == other.lfs_values
             and self.asymmetry_parameter == self.asymmetry_parameter
         )
 
     def __add__(self, other):
+        check_grids(self.lfs_values, other.lfs_values)
+        check_equilibria(self.equilibrium, other.equilibrium)
+
         lfs_values = self.lfs_values + other.lfs_values
+        R_square_diff = self.R_square_diff()
         asymmetry_parameter = (
             np.log(
                 (
-                    self.lfs_values
-                    * np.exp(self.asymmetry_parameter * self.R_square_diff)
+                    self.lfs_values * np.exp(self.asymmetry_parameter * R_square_diff)
                     + other.lfs_values
-                    * np.exp(other.asymmetry_parameter * other.R_square_diff)
+                    * np.exp(other.asymmetry_parameter * R_square_diff)
                 )
                 / (self.lfs_values + other.lfs_values)
             )
-            / self.R_square_diff
+            / R_square_diff
         )
-        return self.__class__(lfs_values, asymmetry_parameter, self.R_square_diff)
+        return self.__class__(lfs_values, asymmetry_parameter, self.equilibrium)
