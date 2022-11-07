ec098764f1decffad8f7d45d181d52e16cbd5c8e
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2022-11-07 17:12:32 +0000

    Initial tests to see whether models run without internal errors. TODO: detailed tests of functionality and possible failures.

---
+++ tests/unit/models/test_bolometer_camera.py
@@ -0,0 +1,5 @@
+import indica.models.bolometer_camera as bolometer_camera
+
+def test_example_bolometer():
+
+    result = bolometer_camera.example_run()
---
+++ tests/unit/models/test_interferometry.py
@@ -0,0 +1,5 @@
+import indica.models.interferometry as interferometry
+
+def test_example_interferometry():
+
+    result = interferometry.example_run()
