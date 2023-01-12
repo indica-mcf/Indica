d384a31ff882431a2dbe21d7f54287e9616d3a11
michael.gemmel <michael.gemmel@tokamakenergy.co.uk>
2023-01-12 16:29:53 +0000

    Made tests into class with setup method

--- tests/integration/models/test_helike_spectroscopy.py
+++ tests/integration/models/test_helike_spectroscopy.py
@@ -24,7 +24,7 @@ def helike_LOS_example(nchannels=3):
             direction[:, 1],
             direction[:, 2],
             name="diagnostic_name",
-            machine_dimensions=plasma.machine_dimensions,
+            machine_dimensions=((0.15, 0.95), (-0.7, 0.7)),
             passes=1,
         )
     elif nchannels == 1:
@@ -36,47 +36,46 @@ def helike_LOS_example(nchannels=3):
         direction[0:1, 1],
         direction[0:1, 2],
         name="diagnostic_name",
-        machine_dimensions=plasma.machine_dimensions,
+        machine_dimensions=((0.15, 0.95), (-0.7, 0.7)),
         passes=1,
     )
     else:
         raise ValueError(f"nchannels: {nchannels} not >= 1")
     return los_transform
 
-plasma = example_plasma()
-single_time_point = plasma.time_to_calculate[0]
-multiple_time_point = plasma.time_to_calculate
 
-multiple_channel_los_transform = helike_LOS_example(nchannels=3)
-single_channel_los_transform = helike_LOS_example(nchannels=1)
+class TestHelike():
+    def setup_class(self):
+        self.plasma = example_plasma()
+        self.single_time_point = self.plasma.time_to_calculate[1]
+        self.multiple_time_point = self.plasma.time_to_calculate
+        self.multiple_channel_los_transform = helike_LOS_example(nchannels=3)
+        self.single_channel_los_transform = helike_LOS_example(nchannels=1)
+        self.single_channel_los_transform.set_equilibrium(self.plasma.equilibrium)
+        self.multiple_channel_los_transform.set_equilibrium(self.plasma.equilibrium)
 
-single_channel_los_transform.set_equilibrium(plasma.equilibrium)
-multiple_channel_los_transform.set_equilibrium(plasma.equilibrium)
+    def test_plasma_time_to_calculate_is_longer_than_one(self):
+        assert self.plasma.time_to_calculate.__len__() > 1
 
-def test_plasma_time_to_calculate_is_longer_than_one():
-    assert plasma.time_to_calculate.__len__() > 1
+    def test_helike_runs_with_example_plasma_and_multiple_LOS(self):
+        model = helike.Helike_spectroscopy("diagnostic_name", )
+        model.set_plasma(self.plasma)
+        model.set_los_transform(self.multiple_channel_los_transform)
+        bckc = model(calc_spectra=False)
+        assert bckc
 
-def test_helike_runs_with_example_plasma_and_multiple_LOS():
-    model = helike.Helike_spectroscopy("diagnostic_name", )
-    model.set_plasma(plasma)
-    model.set_los_transform(multiple_channel_los_transform)
-    bckc = model(calc_spectra=False)
-    assert bckc
+    def test_helike_runs_with_example_plasma_and_single_LOS_and_multiple_time_point(self):
+        model = helike.Helike_spectroscopy("diagnostic_name", )
+        model.set_plasma(self.plasma)
+        self.plasma.time_to_calculate = self.multiple_time_point
+        model.set_los_transform(self.single_channel_los_transform)
+        bckc = model(calc_spectra=False)
+        assert bckc
 
-def test_helike_runs_with_example_plasma_and_single_LOS_and_multiple_time_point():
-    model = helike.Helike_spectroscopy("diagnostic_name", )
-    model.set_plasma(plasma)
-    plasma.time_to_calculate = multiple_time_point
-    model.set_los_transform(single_channel_los_transform)
-    bckc = model(calc_spectra=False)
-    assert bckc
-
-def test_helike_runs_with_example_plasma_and_single_LOS_and_single_time_point():
-    model = helike.Helike_spectroscopy("diagnostic_name", )
-    model.set_plasma(plasma)
-    plasma.time_to_calculate = single_time_point
-    model.set_los_transform(single_channel_los_transform)
-    bckc = model(calc_spectra=False)
-    assert bckc
-
-print()
+    def test_helike_runs_with_example_plasma_and_single_LOS_and_single_time_point(self):
+        model = helike.Helike_spectroscopy("diagnostic_name", )
+        model.set_plasma(self.plasma)
+        self.plasma.time_to_calculate = self.single_time_point
+        model.set_los_transform(self.single_channel_los_transform)
+        bckc = model(calc_spectra=False)
+        assert bckc
