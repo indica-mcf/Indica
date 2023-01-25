06bff61ad294ed93623fae53c68b795484db7798
michael.gemmel <michael.gemmel@tokamakenergy.co.uk>
2023-01-25 14:56:07 +0000

    stashing

--- indica/bayesmodels.py
+++ indica/bayesmodels.py
@@ -1,20 +1,19 @@
 from copy import deepcopy
 import flatdict
 import numpy as np
-
-
 np.seterr(divide="ignore")
+import warnings
+warnings.simplefilter("ignore", category=FutureWarning)
 
+from scipy.stats import uniform
 
 def gaussian(x, mean, sigma):
     return 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(-1 / 2 * ((x - mean) / sigma) ** 2)
 
 
-def uniform(x, lower, upper):
-    if (x > lower) & (x < upper):
-        return 1
-    else:
-        return 0
+def get_uniform(lower, upper):
+    # Less confusing parameterisation of scipy.stats uniform
+    return uniform(loc=lower, scale=upper-lower)
 
 
 class BayesModels:
@@ -81,7 +80,8 @@ class BayesModels:
         return ln_likelihood
 
     def _ln_prior(self, parameters: dict):
-        # TODO: check for conditional priors before giving warning
+        # TODO: check for conditional priors before giving warning /
+        #  Beware if conditional prior is given when only 1 of its parameters is searched it will give too few args
         # params_without_priors = [x for x in parameters.keys() if x not in self.priors.keys()]
         # if params_without_priors.__len__() > 0:
         #     print(f"paramaters {params_without_priors} have no priors assigned")
@@ -92,10 +92,41 @@ class BayesModels:
             if param_values.__len__() == 0:  # if prior assigned but no parameter then skip
                 continue
             elif param_values.__len__() >= 1:
-                ln_prior += np.log(prior_func(*param_values))
+                if hasattr(prior_func, "pdf"):  # for scipy.stats objects use pdf / for lambda functions just call
+                    ln_prior += np.log(prior_func.pdf(*param_values))
+                else:
+                    # if lambda prior with 2+ args is defined when only 1 of its parameters is given ignore it
+                    if prior_func.__code__.co_argcount != param_values.__len__():
+                        continue
+                    else:
+                        ln_prior += np.log(prior_func(*param_values))
             else:
                 raise ValueError(f"Unexpected value for {param_values}")
         return ln_prior
+
+    def sample_from_priors(self, param_names, size=10):
+        #  Use priors to generate samples
+        #  Through out samples that don't meet conditional priors and redraw
+        for name in param_names:
+            if name in self.priors.keys():
+                if hasattr(self.priors[name], "rvs"):
+                    continue
+                else:
+                    raise TypeError(f"prior object {name} missing rvs method")
+            else:
+                raise ValueError(f"Missing prior for {name}")
+
+        samples = np.empty((param_names.__len__(), 0))
+        while samples.size < param_names.__len__() * size:
+            # Some mangling of dictionaries so _ln_prior works
+            new_sample = {name: self.priors[name].rvs(size=size) for name in param_names}
+            ln_prior = self._ln_prior(new_sample)
+            # Convert back from dictionary of arrays to array where ln_prior is finite
+            accepted_samples = np.array(list(new_sample.values()))[:, ln_prior!=-np.inf]
+            samples = np.append(samples, accepted_samples, axis=1)
+        samples = samples[:,0:size]
+        return samples.transpose()
+
 
     def ln_posterior(self, parameters: dict, **kwargs):
         """
