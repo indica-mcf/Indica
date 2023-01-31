db0ab668a6409327218ebe41f44f158d1c992e26
michael.gemmel <michael.gemmel@tokamakenergy.co.uk>
2023-01-31 11:44:11 +0000

    Updated to use bayesmodels.sample_from_priors

--- tests/integration/test_bayesmodels.py
+++ tests/integration/test_bayesmodels.py
@@ -1,4 +1,4 @@
-from indica.bayesmodels import BayesModels, uniform
+from indica.bayesmodels import BayesModels, get_uniform
 
 from indica.models.plasma import example_run
 from indica.models.helike_spectroscopy import Helike_spectroscopy
@@ -7,6 +7,7 @@ from tests.integration.models.test_helike_spectroscopy import helike_LOS_example
 import flatdict
 import numpy as np
 import emcee
+
 
 class TestBayesModels():
 
@@ -22,10 +23,10 @@ class TestBayesModels():
         xrcs.set_los_transform(self.los_transform)
 
         priors = {
-            "Te_prof_y0": lambda x: uniform(x, 500, 1e4),
-            # "Te_prof_peaking": lambda x: uniform(x, 1, 10),
-            "Ti_prof_y0": lambda x: uniform(x, 500, 2e4),
-            # "Ti_prof_peaking": lambda x: uniform(x, 1, 10),
+            "Te_prof.y0": get_uniform(2e3, 5e3),
+            # "Te_prof_peaking": get_uniform(1, 5),
+            "Ti_prof.y0": get_uniform(2e3, 8e3),
+            # "Ti_prof_peaking": get_uniform(1, 5),
         }
 
         bckc = {}
@@ -41,37 +42,28 @@ class TestBayesModels():
         )
 
         # Setup Optimiser
-        params_names = [
+        param_names = [
             "Te_prof.y0",
             # "Te_prof.peaking",
             "Ti_prof.y0",
             # "Ti_prof.peaking",
         ]
-        nwalk = 4
 
-        Te_y0 = np.random.uniform(1000, 5e3, size=(nwalk,1,), )
-        Te_peaking = np.random.uniform(2, 5, size=(nwalk,1,), )
-        Ti_y0 = np.random.uniform(1000, 5e3, size=(nwalk,1,), )
-        Ti_peaking = np.random.uniform(2, 5, size=(nwalk,1,), )
-
-        start_points = np.concatenate([
-                Te_y0,
-                # Te_peaking,
-                Ti_y0,
-                # Ti_peaking,
-            ], axis=1,)
-
-        nwalkers, ndim = start_points.shape
+        ndim = param_names.__len__()
+        nwalkers = ndim * 2
+        start_points = bm.sample_from_priors(param_names, size=nwalkers)
 
         move = [emcee.moves.StretchMove()]
         sampler = emcee.EnsembleSampler(
             nwalkers,
             ndim,
             log_prob_fn=bm.ln_posterior,
-            parameter_names=params_names,
+            parameter_names=param_names,
             moves=move,
+            kwargs={"minimum_lines": True}
         )
         sampler.run_mcmc(start_points, 10, progress=False)
+
 
 if __name__ == "__main__":
     test = TestBayesModels()
