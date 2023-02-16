63f5cf14a8195f56e25927fc5c37ee264a3890fb
Kingsley Collie <kingsley.collie@ukaea.uk>
2023-02-16 17:09:11 +0000

    Plot back-fitted diagnostic profiles

--- bayesian_plot.py
+++ bayesian_plot.py
@@ -3,6 +3,11 @@ import pickle
 import matplotlib.pyplot as plt
 import xarray as xr
 
+from indica.converters import FluxSurfaceCoordinates
+from indica.converters import ImpactParameterCoordinates
+
+t_index = 1
+
 # read run data
 with open("stan_model_data.pkl", "rb") as pkl_file:
     pre_computed = pickle.load(pkl_file)
@@ -15,6 +20,7 @@ bolo_los_data = pre_computed["bolo_los_data"]
 
 draws = xr.open_dataset("bayesian_samples.nc")
 
+# plot the positive midplane density and asymmetry parameter
 fig, (ax0, ax1) = plt.subplots(2, 1, sharex=True)
 plt.title("Full sampling Results")
 
@@ -38,4 +44,64 @@ plt.xlabel("rho")
 ax0.grid(True)
 ax1.grid(True)
 
+plt.show()
+
+# Pinch the equilibrium for impact parameters
+# TODO: find a sensible way to do this
+los_coords = pre_computed["sxr_los_data"].los_values.attrs["transform"]
+equilibrium = los_coords.equilibrium
+flux_coords = FluxSurfaceCoordinates(kind="poloidal")
+flux_coords.set_equilibrium(equilibrium)
+ip_coords = ImpactParameterCoordinates(
+    los_coords, flux_coords, times=pre_computed["sxr_los_data"].los_values.t
+)
+
+plt.figure()
+plt.plot(
+    ip_coords.rho_min.isel(t=t_index),
+    pre_computed["sxr_los_data"].los_values.isel(t=t_index),
+    ".",
+    label="Data",
+)
+plt.plot(
+    ip_coords.rho_min.isel(t=t_index),
+    draws.predicted_sxr_los_vals.mean(dim=("chain", "draw")),
+    label="Back-fit",
+)
+plt.grid()
+plt.legend()
+plt.xlabel("Rho min")
+plt.title("SXR Profile Reconstruction")
+plt.show()
+
+los_coords = pre_computed["bolo_los_data"].los_values.attrs["transform"]
+equilibrium = los_coords.equilibrium
+flux_coords = FluxSurfaceCoordinates(kind="poloidal")
+flux_coords.set_equilibrium(equilibrium)
+ip_coords = ImpactParameterCoordinates(
+    los_coords, flux_coords, times=pre_computed["bolo_los_data"].los_values.t
+)
+
+# Take only non-dropped rho_min values
+# TODO: this is particularly disgusting
+dropped_coords = set(
+    pre_computed["bolo_los_data"]
+    .los_values.dropped.isel(t=t_index)
+    .bolo_kb5v_coords.data
+)
+all_coords = set(ip_coords.rho_min.isel(t=t_index).bolo_kb5v_coords.data)
+selected_coords = list(all_coords - dropped_coords)
+rho_min = ip_coords.rho_min.isel(t=t_index).sel(bolo_kb5v_coords=selected_coords)
+
+plt.figure()
+plt.plot(
+    rho_min, pre_computed["bolo_los_data"].los_values.isel(t=t_index), ".", label="Data"
+)
+plt.plot(
+    rho_min, draws.predicted_bolo_los_vals.mean(dim=("chain", "draw")), label="Back-fit"
+)
+plt.grid()
+plt.legend()
+plt.xlabel("Rho min")
+plt.title("BOLO Profile Reconstruction")
 plt.show()
