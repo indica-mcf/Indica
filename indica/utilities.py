1c2cff5627188a4501437ba8d0ebebbac8611caa
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2023-02-21 09:59:33 +0000

    General methods for line intersections + new methods to assign data and datatypes to DataArray

--- indica/utilities.py
+++ indica/utilities.py
@@ -20,6 +20,7 @@ from xarray.core.dataset import Dataset
 from xarray.core.variable import Variable
 
 from .numpy_typing import ArrayLike
+from .numpy_typing import LabeledArray
 from .numpy_typing import OnlyArray
 
 
@@ -257,3 +258,135 @@ def input_check(
         and (sliced_var_to_check.ndim != ndim_to_check)
     ):
         raise ValueError(f"{var_name} must have {ndim_to_check} dimensions.")
+
+
+def assign_datatype(data_array: DataArray, datatype: tuple, unit=""):
+    data_array.name = f"{datatype[1]}_{datatype[0]}"
+    data_array.attrs["datatype"] = datatype
+    if len(unit) > 0:
+        data_array.attrs["unit"] = unit
+
+
+def assign_data(
+    data: LabeledArray,
+    datatype: tuple,
+    units: str = "",
+    make_copy=True,
+    coords: list = None,
+    long_name: str = None,
+):
+    new_data: DataArray
+
+    if make_copy:
+        new_data = deepcopy(data)
+    else:
+        new_data = data
+
+    if type(new_data) is not DataArray and coords is not None:
+        new_data = DataArray(new_data, coords)
+
+    new_data.name = f"{datatype[1]}_{datatype[0]}"
+    datatype0 = datatype[0].replace("_", " ")
+    datatype1 = datatype[1].replace("_", " ")
+    datatype1 = f"{str.upper(datatype1[0])}{datatype1[1:]}"
+
+    if long_name is not None:
+        new_data.attrs["long_name"] = long_name
+    else:
+        new_data.attrs["long_name"] = f"{datatype1} {datatype0}"
+
+    new_data.attrs["datatype"] = datatype
+
+    if len(units) > 0:
+        new_data.attrs["units"] = units
+
+    return new_data
+
+
+def print_like(string: str):
+    print(f"\n {string}")
+
+
+def get_function_name():
+    return str(inspect.stack()[1][3])
+
+
+def _rect_inter_inner(x1, x2):
+    n1 = x1.shape[0] - 1
+    n2 = x2.shape[0] - 1
+    X1 = np.c_[x1[:-1], x1[1:]]
+    X2 = np.c_[x2[:-1], x2[1:]]
+    S1 = np.tile(X1.min(axis=1), (n2, 1)).T
+    S2 = np.tile(X2.max(axis=1), (n1, 1))
+    S3 = np.tile(X1.max(axis=1), (n2, 1)).T
+    S4 = np.tile(X2.min(axis=1), (n1, 1))
+    return S1, S2, S3, S4
+
+
+def _rectangle_intersection_(x1, y1, x2, y2):
+    S1, S2, S3, S4 = _rect_inter_inner(x1, x2)
+    S5, S6, S7, S8 = _rect_inter_inner(y1, y2)
+
+    C1 = np.less_equal(S1, S2)
+    C2 = np.greater_equal(S3, S4)
+    C3 = np.less_equal(S5, S6)
+    C4 = np.greater_equal(S7, S8)
+
+    ii, jj = np.nonzero(C1 & C2 & C3 & C4)
+    return ii, jj
+
+
+def intersection(x1, y1, x2, y2):
+    """
+    INTERSECTIONS Intersections of curves.
+       Computes the (x,y) locations where two curves intersect.  The curves
+       can be broken with NaNs or have vertical segments.
+    usage:
+    x,y=intersection(x1,y1,x2,y2)
+        Example:
+        a, b = 1, 2
+        phi = np.linspace(3, 10, 100)
+        x1 = a*phi - b*np.sin(phi)
+        y1 = a - b*np.cos(phi)
+        x2=phi
+        y2=np.sin(phi)+2
+        x,y, ix, iy=intersection(x1,y1,x2,y2)
+        plt.plot(x1,y1,c='r')
+        plt.plot(x2,y2,c='g')
+        plt.plot(x,y,'*k')
+        plt.show()
+    """
+    ii, jj = _rectangle_intersection_(x1, y1, x2, y2)
+    n = len(ii)
+
+    dxy1 = np.diff(np.c_[x1, y1], axis=0)
+    dxy2 = np.diff(np.c_[x2, y2], axis=0)
+
+    T = np.zeros((4, n))
+    AA = np.zeros((4, 4, n))
+    AA[0:2, 2, :] = -1
+    AA[2:4, 3, :] = -1
+    AA[0::2, 0, :] = dxy1[ii, :].T
+    AA[1::2, 1, :] = dxy2[jj, :].T
+
+    BB = np.zeros((4, n))
+    BB[0, :] = -x1[ii].ravel()
+    BB[1, :] = -x2[jj].ravel()
+    BB[2, :] = -y1[ii].ravel()
+    BB[3, :] = -y2[jj].ravel()
+
+    for i in range(n):
+        try:
+            T[:, i] = np.linalg.solve(AA[:, :, i], BB[:, i])
+        except np.linalg.LinAlgError:
+            T[:, i] = np.NaN
+
+    in_range = (T[0, :] >= 0) & (T[1, :] >= 0) & (T[0, :] <= 1) & (T[1, :] <= 1)
+
+    xy0 = T[2:, in_range]
+    xy0 = xy0.T
+
+    indii = ii[in_range] + T[0, in_range]
+    indjj = jj[in_range] + T[1, in_range]
+
+    return xy0[:, 0], xy0[:, 1], indii, indjj
