d5cd1d8469b5337c2fba2467645fa0d82311362a
Marco Sertoli <marco.sertoli@outlook.com>
2023-04-06 00:11:40 +0100

    Fixing readers: attempt N

--- indica/readers/__init__.py
+++ indica/readers/__init__.py
@@ -8,9 +8,6 @@ functionality for a different format of data.
 from .abstractreader import DataReader
 from .adas import ADASReader
 from .ppfreader import PPFReader
+from .st40reader import ST40Reader
 
-__all__ = [
-    "ADASReader",
-    "DataReader",
-    "PPFReader",
-]
+__all__ = ["ADASReader", "DataReader", "PPFReader", "ST40Reader"]
