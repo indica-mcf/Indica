02a3f8224566ce6cd818c2fadb5021ec8ce5ff25
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2023-04-05 21:59:50 +0100

    Testing whether deleting st40reader from readers __init__ solves the problem

--- indica/readers/__init__.py
+++ indica/readers/__init__.py
@@ -10,13 +10,11 @@ from .adas import ADASReader
 from .ppfreader import PPFReader
 from .selectors import choose_on_plot
 from .selectors import DataSelector
-from .st40reader import ST40Reader
 
 __all__ = [
     "ADASReader",
     "DataReader",
     "PPFReader",
-    "ST40Reader",
     "choose_on_plot",
     "DataSelector",
 ]
