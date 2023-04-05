20d140dc845211fc010b089a54c5e5222f22d697
Marco Sertoli <marco.sertoli@outlook.com>
2023-04-05 23:21:29 +0100

    Fixing readers: attempt 4

--- indica/readers/ppfreader.py
+++ indica/readers/ppfreader.py
@@ -24,8 +24,7 @@ from sal.core.exception import NodeNotFound
 from sal.dataclass import Signal
 import scipy.constants as sc
 
-# import surf_los as surf_los
-from .surf_los import read_surf_los
+import surf_los as surf_los
 from .abstractreader import CACHE_DIR
 from .abstractreader import DataReader
 from .. import session
@@ -34,7 +33,7 @@ from ..numpy_typing import RevisionLike
 from ..utilities import to_filename
 
 
-SURF_PATH = Path(read_surf_los.__file__).parent / "surf_los.dat"
+SURF_PATH = Path(surf_los.__file__).parent / "surf_los.dat"
 
 
 class PPFError(Exception):
@@ -426,7 +425,7 @@ class PPFReader(DataReader):
         dl: float = 0.005,
     ) -> Dict[str, Any]:
         """Fetch raw data for electron cyclotron emissin diagnostics."""
-        _, _, zstart, zend, _, _ = read_surf_los(
+        _, _, zstart, zend, _, _ = surf_los.surf_los(
             SURF_PATH, self.pulse, instrument.lower()
         )
         assert zstart[0] == zend[0]
@@ -519,7 +518,7 @@ class PPFReader(DataReader):
                 results[q] = np.array(luminosities).T
             results[q + "_error"] = self._default_error * results[q]
             results[q + "_records"] = records
-            xstart, xend, zstart, zend, ystart, yend = read_surf_los(
+            xstart, xend, zstart, zend, ystart, yend = surf_los.surf_los(
                 SURF_PATH, self.pulse, instrument.lower() + "/" + q.lower()
             )
             results[q + "_xstart"] = xstart[channels]
