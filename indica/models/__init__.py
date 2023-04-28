4abfc77894dec10904a5cf7c04c69f1cfd4a88d1
marcosertoli <74655352+marcosertoli@users.noreply.github.com>
2023-04-28 14:58:47 +0100

    Marcosertoli/fix model example runs (#233)
    * Fixed example_run in diagnostic models.
    
    * Added __init__ to /models.
    
    * Update __init__.py
    
    Fixed comma :-)
    
    * Fixed pre-commit
    
    ---------
    
    Co-authored-by: Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>

---
+++ indica/models/__init__.py
@@ -0,0 +1,11 @@
+__all__ = [
+    "bolometer_camera",
+    "charge_exchange",
+    "diode_filters",
+    "equilibrium_reconstruction",
+    "helike_spectroscopy",
+    "interferometry",
+    "plasma",
+    "sxr_camera",
+    "thomson_scattering",
+]
