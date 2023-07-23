78731e0aafbb5cff118703f0afbce050e2f2f605
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2023-07-24 00:37:52 +0100

    Formatting mod to pass pre-commit

--- indica/workflows/bayes_workflow.py
+++ indica/workflows/bayes_workflow.py
@@ -250,7 +250,7 @@ def plot_bayes_result(
     plot_profile(
         blobs[key], key, figheader=figheader, phantom_profile=phantom_profiles[key]
     )
-            
+
     key = "electron_temperature"
     plot_profile(
         blobs[key],
