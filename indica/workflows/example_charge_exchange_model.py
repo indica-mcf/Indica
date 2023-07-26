e8628d1ecbebb11635f18b65c2502498b921d81e
jonathan.wood <jonathan.wood@tokamakenergy.co.uk>
2023-07-26 15:07:43 +0100

    Adding plotting option to example workflow

--- indica/workflows/example_charge_exchange_model.py
+++ indica/workflows/example_charge_exchange_model.py
@@ -8,6 +8,10 @@ from indica.models.plasma import example_run
 from indica.models.neutral_beam import NeutralBeam
 from indica.models.charge_exchange import ChargeExchange
 
+from matplotlib import pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+
 
 # Workflow for running FIDASIM
 def run_fidasim(
@@ -16,6 +20,7 @@ def run_fidasim(
     tend: float,
     dt: float,
     call_fidasim: bool = False,
+    plot: bool = True,
 ):
 
     # Read ST40 data
@@ -45,6 +50,45 @@ def run_fidasim(
     # ToDo: activate this method
     # Call FIDASIM
     #cxspec(method='fidasim', run_fidasim=call_fidasim)
+
+    # Plotting
+    if plot:
+        cols_time = cm.gnuplot2(np.linspace(0.1, 0.75, len(plasma.t), dtype=float))
+
+        plt.close('all')
+
+        plt.figure()
+        for i, t in enumerate(plasma.t.values):
+            plasma.toroidal_rotation.sel(t=t, element='c').plot(
+                color=cols_time[i],
+                label=f"t={t:1.2f} s",
+                alpha=0.7,
+            )
+            Vtor = cxspec.Vtor_at_channels.sel(t=t, method="nearest")
+            omega_tor = Vtor * 1e3 / cxspec.transect_transform.R.values
+            plt.scatter(
+                Vtor.rho_poloidal, omega_tor, color=cols_time[i], marker="o", alpha=0.7
+            )
+        plt.xlabel("rho")
+        plt.ylabel("Measured toroidal rotation (rad/s)")
+        plt.ylim([0.0, 0.6e6])
+        plt.legend()
+
+        plt.figure()
+        for i, t in enumerate(plasma.t.values):
+            plasma.ion_temperature.sel(t=t, element='c').plot(
+                color=cols_time[i],
+                label=f"t={t:1.2f} s",
+                alpha=0.7,
+            )
+            Ti = cxspec.Ti_at_channels.sel(t=t, method="nearest")
+            plt.scatter(Ti.rho_poloidal, Ti, color=cols_time[i], marker="o", alpha=0.7)
+        plt.xlabel("rho")
+        plt.ylabel("Measured ion temperature (eV)")
+        plt.ylim([0.0, 7000])
+        plt.legend()
+
+        plt.show(block=True)
 
     return
 
