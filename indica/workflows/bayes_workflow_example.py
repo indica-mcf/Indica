82d6fee53c41c21ea51fe8c64a44aa05f22cbaea
michael gemmell <michael.gemmell@tokamakenergy.co.uk>
2023-08-02 13:20:28 +0100

    renaming to BayesWorkflowExample

--- indica/workflows/bayes_workflow_example.py
+++ indica/workflows/bayes_workflow_example.py
@@ -74,59 +74,60 @@ DEFAULT_PRIORS = {
 }
 
 OPTIMISED_PARAMS = [
-    "Ne_prof.y1",
+    # "Ne_prof.y1",
     "Ne_prof.y0",
-    "Ne_prof.peaking",
+    # "Ne_prof.peaking",
     # "Ne_prof.wcenter",
     # "Ne_prof.wped",
     # "ar_conc",
     # "Nimp_prof.y1",
     "Nimp_prof.y0",
     # "Nimp_prof.wcenter",
-    "Nimp_prof.wped",
-    "Nimp_prof.peaking",
+    # "Nimp_prof.wped",
+    # "Nimp_prof.peaking",
     "Te_prof.y0",
     # "Te_prof.wped",
     "Te_prof.wcenter",
     "Te_prof.peaking",
     "Ti_prof.y0",
-    # "Ti_prof.wped",
+    "Ti_prof.wped",
     "Ti_prof.wcenter",
     "Ti_prof.peaking",
 ]
 
 OPTIMISED_QUANTITY = [
-                        "xrcs.spectra",
-                        "cxff_pi.ti",
-                        "efit.wp",
-                        "smmh1.ne"]
+    "xrcs.spectra",
+    "cxff_pi.ti",
+    "efit.wp",
+    "smmh1.ne",
+]
 
 
-class ExampleBayesWorkflow(AbstractBayesWorkflow):
+class BayesWorkflowExample(AbstractBayesWorkflow):
     def __init__(
             self,
-            pulse=None,
-            pulse_to_write=None,
-            run="RUN01",
-            diagnostics=None,
-            param_names=None,
-            opt_quantity=None,
-            priors = None,
-            phantoms=False,
-            phantom_params=None,
-            model_kwargs = None,
+            pulse: int = None,
+            pulse_to_write: bool = None,
+            run: str = "RUN01",
+            diagnostics: list = None,
+            param_names: list = None,
+            opt_quantity: list = None,
+            priors: dict = None,
+            profile_params: dict = None,
+            phantoms: bool = False,
+            model_kwargs: dict = {},
 
-            nwalkers=50,
-            tstart=0.02,
-            tend=0.10,
-            dt=0.01,
-            tsample=0.06,
-            iterations=100,
-            burn_frac=0,
+            nwalkers: int = 50,
+            tstart: float = 0.02,
+            tend: float = 0.10,
+            dt: float = 0.01,
+            tsample: float = 0.06,
+            iterations: int = 100,
+            burn_frac: float = 0,
 
-            mds_write=False,
-            plot=True,
-            sample_high_density = False,
+            mds_write: bool = False,
+            plot: bool = True,
+            sample_high_density: bool = False,
     ):
         self.pulse = pulse
         self.pulse_to_write = pulse_to_write
@@ -135,7 +136,7 @@ class ExampleBayesWorkflow(AbstractBayesWorkflow):
         self.param_names = param_names
         self.opt_quantity = opt_quantity
         self.priors = priors
-        self.phantom_params = phantom_params
+        self.profile_params = profile_params
         self.model_kwargs = model_kwargs
         self.phantoms = phantoms
 
@@ -151,13 +152,13 @@ class ExampleBayesWorkflow(AbstractBayesWorkflow):
         self.plot = plot
         self.sample_high_density = sample_high_density
 
-        for attribute in ["pulse", "param_names", "opt_quantity", "priors", "diagnostics", "phantom_params"]:
+        for attribute in ["pulse", "param_names", "opt_quantity", "priors", "diagnostics", "profile_params"]:
             if getattr(self, attribute) is None:
                 raise ValueError(f"{attribute} needs to be defined")
 
+        self.read_data(self.diagnostics)
         self.setup_plasma()
         self.save_phantom_profiles()
-        self.read_data(self.diagnostics)
         self.setup_models(self.diagnostics)
         self.setup_opt_data(phantoms=self.phantoms)
         self.setup_optimiser(self.model_kwargs)
@@ -179,7 +180,8 @@ class ExampleBayesWorkflow(AbstractBayesWorkflow):
         self.plasma.time_to_calculate = self.plasma.t[
             np.abs(self.tsample - self.plasma.t).argmin()
         ]
-        self.plasma.update_profiles(self.phantom_params)
+        self.plasma.set_equilibrium(self.reader.equilibrium)
+        self.plasma.update_profiles(self.profile_params)
         self.plasma.build_atomic_data(calc_power_loss=False)
 
     def setup_models(self, diagnostics: list):
@@ -228,33 +230,30 @@ class ExampleBayesWorkflow(AbstractBayesWorkflow):
             else:
                 raise ValueError(f"{diag} not found in setup_models")
             model.plasma = self.plasma
-
-
             self.models[diag] = model
 
     def setup_opt_data(self, phantoms=False):
         if phantoms:
-            self._phantom_data()
+            self.opt_data = self._phantom_data()
         else:
-            self._exp_data()
+            self.opt_data = self._exp_data()
 
     def _phantom_data(self, noise=False, noise_factor=0.1):
-        self.opt_data = {}
+        opt_data = {}
         if "smmh1" in self.diagnostics:
-            self.opt_data["smmh1.ne"] = (
+            opt_data["smmh1.ne"] = (
                 self.models["smmh1"]()
                     .pop("ne")
                     .expand_dims(dim={"t": [self.plasma.time_to_calculate]})
             )
         if "xrcs" in self.diagnostics:
-
-            self.opt_data["xrcs.spectra"] = (
+            opt_data["xrcs.spectra"] = (
                 self.models["xrcs"]()
                     .pop("spectra")
                     .expand_dims(dim={"t": [self.plasma.time_to_calculate]})
             )
-            self.opt_data["xrcs.spectra"]["error"] = np.sqrt(
-                self.opt_data["xrcs.spectra"]
+            opt_data["xrcs.spectra"]["error"] = np.sqrt(
+                opt_data["xrcs.spectra"]
             )
         if "cxff_pi" in self.diagnostics:
             cxrs_data = (
@@ -262,61 +261,56 @@ class ExampleBayesWorkflow(AbstractBayesWorkflow):
                     .pop("ti")
                     .expand_dims(dim={"t": [self.plasma.time_to_calculate]})
             )
-            self.opt_data["cxff_pi.ti"] = cxrs_data.where(cxrs_data.channel == 2, drop=True)
+            opt_data["cxff_pi.ti"] = cxrs_data.where(cxrs_data.channel == 2, drop=True)
         if "efit" in self.diagnostics:
-            self.opt_data["efit.wp"] = (
+            opt_data["efit.wp"] = (
                 self.models["efit"]()
                     .pop("wp")
                     .expand_dims(dim={"t": [self.plasma.time_to_calculate]})
             )
 
         if noise:
-            self.opt_data["smmh1.ne"] = self.opt_data["smmh1.ne"] + self.opt_data[
+            opt_data["smmh1.ne"] = opt_data["smmh1.ne"] + self.opt_data[
                 "smmh1.ne"
             ].max().values * np.random.normal(0, noise_factor, None)
-            self.opt_data["xrcs.spectra"] = self.opt_data[
-                                                "xrcs.spectra"
-                                            ] + np.random.normal(
+            opt_data["xrcs.spectra"] = opt_data[
+                                           "xrcs.spectra"
+                                       ] + np.random.normal(
                 0,
-                np.sqrt(self.opt_data["xrcs.spectra"].values[0,]),
-                self.opt_data["xrcs.spectra"].shape[1],
+                np.sqrt(opt_data["xrcs.spectra"].values[0,]),
+                opt_data["xrcs.spectra"].shape[1],
             )
-            self.opt_data["cxff_pi.ti"] = self.opt_data[
-                                              "cxff_pi.ti"
-                                          ] + self.opt_data["cxff_pi.ti"].max().values * np.random.normal(
-                0, noise_factor, self.opt_data["cxff_pi.ti"].shape[1]
+            opt_data["cxff_pi.ti"] = opt_data[
+                                         "cxff_pi.ti"
+                                     ] + opt_data["cxff_pi.ti"].max().values * np.random.normal(
+                0, noise_factor, opt_data["cxff_pi.ti"].shape[1]
             )
-            self.opt_data["efit.wp"] = self.opt_data["efit.wp"] + self.opt_data[
+            opt_data["efit.wp"] = opt_data["efit.wp"] + opt_data[
                 "efit.wp"
             ].max().values * np.random.normal(0, noise_factor, None)
-
-        self.phantom_profiles = {}
-        for key in ["electron_density", "impurity_density", "electron_temperature", "ion_temperature",
-                    "ion_density", "fast_density", "neutral_density"]:
-            self.phantom_profiles[key] = getattr(self.plasma, key).sel(
-                t=self.plasma.time_to_calculate
-            ).copy()
+        return opt_data
 
     def _exp_data(self):
-        self.opt_data = flatdict.FlatDict(self.data, ".")
+        opt_data = flatdict.FlatDict(self.data, ".")
         if "xrcs" in self.diagnostics:
-            self.opt_data["xrcs.spectra"]["wavelength"] = (
-                    self.opt_data["xrcs.spectra"].wavelength * 0.1
+            opt_data["xrcs.spectra"]["wavelength"] = (
+                    opt_data["xrcs.spectra"].wavelength * 0.1
             )
-            background = self.opt_data["xrcs.spectra"].where(
-                (self.opt_data["xrcs.spectra"].wavelength < 0.392)
-                & (self.opt_data["xrcs.spectra"].wavelength > 0.388),
+            background = opt_data["xrcs.spectra"].where(
+                (opt_data["xrcs.spectra"].wavelength < 0.392)
+                & (opt_data["xrcs.spectra"].wavelength > 0.388),
                 drop=True,
             )
-            self.models["xrcs"].background = background.mean(dim="wavelength").sel(t=self.plasma.time_to_calculate)
-            self.opt_data["xrcs.spectra"]["error"] = np.sqrt(
-                self.opt_data["xrcs.spectra"] + background.std(dim="wavelength") ** 2
+            self.model_kwargs["xrcs_background"] = background.mean(dim="wavelength").sel(t=self.plasma.time_to_calculate)
+            opt_data["xrcs.spectra"]["error"] = np.sqrt(
+                opt_data["xrcs.spectra"] + background.std(dim="wavelength") ** 2
             )
 
         if "cxff_pi" in self.diagnostics:
-            self.opt_data["cxff_pi"]["ti"] = self.opt_data["cxff_pi"]["ti"].where(
-                self.opt_data["cxff_pi"]["ti"].channel == 2, drop=True
+            opt_data["cxff_pi"]["ti"] = opt_data["cxff_pi"]["ti"].where(
+                opt_data["cxff_pi"]["ti"].channel == 2, drop=True
             )
+        return opt_data
 
     def setup_optimiser(self, model_kwargs):
 
@@ -340,13 +334,14 @@ class ExampleBayesWorkflow(AbstractBayesWorkflow):
         )
 
         if self.sample_high_density:
-            self.start_points = self.bayesopt.sample_from_high_density_region(self.param_names, self.sampler, self.nwalkers)
+            self.start_points = self.bayesopt.sample_from_high_density_region(self.param_names, self.sampler,
+                                                                              self.nwalkers)
         else:
             self.start_points = self.bayesopt.sample_from_priors(
                 self.param_names, size=self.nwalkers
             )
 
-    def __call__(self, filepath = "./results/test/", **kwargs):
+    def __call__(self, filepath="./results/test/", **kwargs):
 
         if self.mds_write:
             # check_analysis_run(self.pulse, self.run)
@@ -368,19 +363,19 @@ class ExampleBayesWorkflow(AbstractBayesWorkflow):
 
 
 if __name__ == "__main__":
-
-    run = ExampleBayesWorkflow(
+    run = BayesWorkflowExample(
         pulse=10009,
         pulse_to_write=23000101,
         run="RUN01",
         diagnostics=["xrcs", "efit", "smmh1", "cxff_pi"],
         opt_quantity=OPTIMISED_QUANTITY,
         param_names=OPTIMISED_PARAMS,
-        phantom_params=DEFAULT_PHANTOM_PARAMS,
+        profile_params=DEFAULT_PHANTOM_PARAMS,
         priors=DEFAULT_PRIORS,
+        model_kwargs={"xrcs_moment_analysis":False, },
 
-        iterations=200,
-        nwalkers=50,
+        iterations=10,
+        nwalkers=20,
         burn_frac=0.10,
         dt=0.005,
         tsample=0.060,
@@ -388,8 +383,6 @@ if __name__ == "__main__":
         mds_write=True,
         plot=True,
         phantoms=True,
-        sample_high_density=True,
-        model_kwargs= {}
+        sample_high_density=False,
     )
-    results = run(filepath="./results/test/",)
-
+    results = run(filepath="./results/test/", )
