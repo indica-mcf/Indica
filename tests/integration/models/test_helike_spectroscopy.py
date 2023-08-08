6fcff7534968ff20ead6f525ff48ce0eaac88cbd
michael gemmell <michael.gemmell@tokamakenergy.co.uk>
2023-08-08 14:59:28 +0100

    refactoring names

--- tests/integration/models/test_helike_spectroscopy.py
+++ tests/integration/models/test_helike_spectroscopy.py
@@ -1,5 +1,5 @@
 import indica.models.helike_spectroscopy as helike
-from indica.models.helike_spectroscopy import helike_LOS_example
+from indica.models.helike_spectroscopy import helike_transform_example
 from indica.models.plasma import example_run as example_plasma
 
 
@@ -8,13 +8,13 @@ class TestHelike:
         self.plasma = example_plasma()  # using Phantom
         self.single_time_point = self.plasma.time_to_calculate[1]
         self.multiple_time_point = self.plasma.time_to_calculate
-        self.multiple_channel_los_transform = helike_LOS_example(nchannels=3)
-        self.single_channel_los_transform = helike_LOS_example(nchannels=1)
+        self.multiple_channel_los_transform = helike_transform_example(nchannels=3)
+        self.single_channel_los_transform = helike_transform_example(nchannels=1)
         self.single_channel_los_transform.set_equilibrium(self.plasma.equilibrium)
         self.multiple_channel_los_transform.set_equilibrium(self.plasma.equilibrium)
 
     def setup_method(self):
-        self.model = helike.Helike_spectroscopy("diagnostic_name")
+        self.model = helike.HelikeSpectrometer("diagnostic_name")
         self.model.set_plasma(self.plasma)
 
     def test_helike_moment_runs_with_multiple_LOS(
