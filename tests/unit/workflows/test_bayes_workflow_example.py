cf1c127c6128b38200011131be7a56df09d82841
michael gemmell <michael.gemmell@tokamakenergy.co.uk>
2023-08-15 10:59:28 +0100

    workflow broken up into methods

--- tests/unit/workflows/test_bayes_workflow_example.py
+++ tests/unit/workflows/test_bayes_workflow_example.py
@@ -12,10 +12,7 @@ from indica.workflows.bayes_workflow_example import (
 
 """
 TODO:
-
-test kwarg handling
-
-Mock reader for testing experimental data methods
+Mock reader for testing experimental data reading
 """
 
 
@@ -34,18 +31,18 @@ class TestBayesWorkflowExample:
             dt=0.005,
         )
         self.plasma_settings = dict(
+         tsample=0.060,
+        )
 
-         tsample=0.060,)
 
-
-        self.opt_settings = dict(
+        self.optimiser_settings = dict(
             model_kwargs = {
                            "xrcs_moment_analysis": False,
                        },
-        iterations = 1,
-        nwalkers = 20,
-        burn_frac = 0.10,
-        sample_high_density = False,
+            iterations = 1,
+            nwalkers = 20,
+            burn_frac = 0.10,
+            sample_high_density = False,
         )
 
         self.call_settings = dict(
@@ -55,11 +52,11 @@ class TestBayesWorkflowExample:
             mds_write = False,
             plot = False,)
 
-        self.untouched_workflow = BayesWorkflowExample(**self.init_settings)
-        self.workflow = copy.deepcopy(self.untouched_workflow)
+        self.workflow_untouched = BayesWorkflowExample(**self.init_settings)
+        self.workflow=None
 
     def setup_method(self):
-        self.workflow = copy.deepcopy(self.untouched_workflow)
+        self.workflow = copy.deepcopy(self.workflow_untouched)
 
     def teardown_method(self):
         self.workflow = None
@@ -79,34 +76,39 @@ class TestBayesWorkflowExample:
     def test_init_phantoms_false_with_example_plasma(self):
         with pytest.raises(ValueError):
             example = BayesWorkflowExample(
-                dict(self.default_settings, **{"phantoms": False})
+                dict(self.init_settings, **{"phantoms": False})
             )
 
     def test_init_not_including_all_required_inputs(self):
         with pytest.raises(ValueError):
             example = BayesWorkflowExample(
-                dict(self.default_settings, **{"param_names": None})
+                dict(self.init_settings, **{"param_names": None})
             )
 
     # def test_reader_has_read_all_diagnostic_data(self):
     #     assert all(diag_name in self.workflow.reader.keys() for diag_name in self.workflow.diagnostics)
 
     def test_plasma_has_equilibrium(self):
-        self.workflow.setup_plasma()
+        self.workflow.setup_plasma(**self.plasma_settings)
         assert hasattr(self.workflow.plasma, "equilibrium")
 
     def test_phantom_profiles_are_not_mutatable(self):
-        self.workflow.setup_plasma()
+        self.workflow.setup_plasma(**self.plasma_settings)
         phantoms = copy.deepcopy(self.workflow.phantom_profiles)
         self.workflow.plasma.electron_temperature += 1
         assert phantoms is not self.workflow.phantom_profiles
 
     def test_setup_models_with_wrong_diagnostic_names(self):
-        with pytest.raises(Exception):
+        with pytest.raises(ValueError):
             self.workflow.setup_models(["foo", "bar", "xrcs"])
 
+    def test_opt_data_without_plasma(self):
+        with pytest.raises(ValueError):
+            self.workflow.setup_opt_data(phantoms=True)
+
     def test_phantom_data_exists(self):
-        self.workflow._phantom_data()
+        self.workflow.setup_plasma(**self.plasma_settings)
+        self.workflow.setup_opt_data(phantoms=True)
         assert self.workflow.opt_data
 
     # def test_experimental_data_exists(self):
@@ -114,7 +116,8 @@ class TestBayesWorkflowExample:
     #     assert self.workflow.opt_data
 
     def test_phantom_data_has_time_dim(self):
-        self.workflow._phantom_data()
+        self.workflow.setup_plasma(**self.plasma_settings)
+        self.workflow.setup_opt_data(phantoms=True)
         for key, value in self.workflow.opt_data.items():
             assert "t" in value.dims
 
@@ -124,21 +127,26 @@ class TestBayesWorkflowExample:
     #       assert "t" in value.dims
 
     def test_phantom_data_runs_with_noise_added(self):
-        self.workflow._phantom_data(noise=True)
+        self.workflow.setup_plasma(**self.plasma_settings)
+        self.workflow.setup_opt_data(phantoms=True, noise=True)
         assert self.workflow.opt_data
 
-    def test_sampling_from_high_density(self):
-        self.workflow._sample_start_points(sample_high_density=True)
+    def test_sampling_from_priors(self):
+        self.workflow.setup_plasma(**self.plasma_settings)
+        self.workflow.setup_opt_data(phantoms=True, )
+        self.workflow.setup_optimiser(**dict(self.optimiser_settings, **{"sample_high_density":False}))
         assert True
 
-    def test_sampling_from_priors(self):
-        self.workflow._sample_start_points(sample_high_density=False)
+    def test_sampling_from_high_density(self):
+        self.workflow.setup_plasma(**self.plasma_settings)
+        self.workflow.setup_opt_data(phantoms=True, )
+        self.workflow.setup_optimiser(**dict(self.optimiser_settings, **{"sample_high_density":True}))
         assert True
 
     def test_worklow_has_results_after_run(self):
-        self.workflow.setup_plasma()
-        self.workflow.setup_opt_data(self.workflow.phantoms)
-        self.workflow.setup_optimiser()
+        self.workflow.setup_plasma(**self.plasma_settings)
+        self.workflow.setup_opt_data(phantoms=True)
+        self.workflow.setup_optimiser(**self.optimiser_settings)
         self.workflow.run_sampler()
         if not hasattr(self.workflow, "result"):
             raise ValueError(f"missing result in workflow object")
