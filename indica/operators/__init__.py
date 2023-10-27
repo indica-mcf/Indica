52fd84bb3656fac0450323fda36af23bc05a540e
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2023-10-27 15:32:32 +0100

    Deleting all unused stuff in st40 branch

--- indica/operators/__init__.py
+++ indica/operators/__init__.py
@@ -2,9 +2,6 @@ from .abstractoperator import Operator
 from .abstractoperator import OperatorError
 from .atomic_data import FractionalAbundance
 from .atomic_data import PowerLoss
-from .centrifugal_asymmetry import AsymmetryParameter
-from .centrifugal_asymmetry import ToroidalRotation
-from .mean_charge import MeanCharge
 from .zeff import CalcZeff
 
 __all__ = [
@@ -12,8 +9,5 @@ __all__ = [
     "OperatorError",
     "FractionalAbundance",
     "PowerLoss",
-    "ToroidalRotation",
-    "AsymmetryParameter",
-    "MeanCharge",
     "CalcZeff",
 ]
