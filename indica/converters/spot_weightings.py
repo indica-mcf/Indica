509ddeae85b8769b60cd7315a696efbd5ee729cb
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2023-12-18 17:15:08 +0000

    Ran pre-commit

--- indica/converters/spot_weightings.py
+++ indica/converters/spot_weightings.py
@@ -1,38 +1,25 @@
 """Definition of weighting functions for line of sight spots.
 """
 
-import getpass
-from typing import cast
-from typing import Tuple
-
-import matplotlib.pylab as plt
+# import matplotlib.pylab as plt
 import numpy as np
-import xarray as xr
-from xarray import DataArray
-from xarray import Dataset
-from xarray import zeros_like
 
-from .abstractconverter import Coordinates
-from .abstractconverter import CoordinateTransform
-from .abstractconverter import find_wall_intersections
 from .line_of_sight import LineOfSightTransform
-from ..numpy_typing import LabeledArray
-from ..numpy_typing import OnlyArray
+
+# import xarray as xr
 
 
-class SpotWeightings():
-    """
-
-    """
+class SpotWeightings:
+    """ """
 
     def __init__(
-            self,
-            los_transform: LineOfSightTransform,
-            dist_func: str,
-            sigma_w: float = 0.01,
-            sigma_v: float = 0.01,
-            p_w: float = 1.,
-            p_v: float = 1.,
+        self,
+        los_transform: LineOfSightTransform,
+        dist_func: str,
+        sigma_w: float = 0.01,
+        sigma_v: float = 0.01,
+        p_w: float = 1.0,
+        p_v: float = 1.0,
     ):
         self.los_transform = los_transform
         self.dist_func = dist_func
@@ -43,9 +30,19 @@ class SpotWeightings():
 
         n_w = int(np.sqrt(los_transform.beamlets))
         n_v = int(np.sqrt(los_transform.beamlets))
-        grid_w = np.linspace(-los_transform.spot_width / 2, los_transform.spot_width / 2, n_w * 2 + 1, dtype=float)
+        grid_w = np.linspace(
+            -los_transform.spot_width / 2,
+            los_transform.spot_width / 2,
+            n_w * 2 + 1,
+            dtype=float,
+        )
         grid_w = grid_w[1::2]
-        grid_v = np.linspace(-los_transform.spot_height / 2, los_transform.spot_height / 2, n_v * 2 + 1, dtype=float)
+        grid_v = np.linspace(
+            -los_transform.spot_height / 2,
+            los_transform.spot_height / 2,
+            n_v * 2 + 1,
+            dtype=float,
+        )
         grid_v = grid_v[1::2]
         W, V = np.meshgrid(grid_w, grid_v)
 
@@ -63,11 +60,6 @@ class SpotWeightings():
 
     def super_gaussian(self):
         self.weightings = np.exp(
-            - ((self.W)**2/(2 * self.sigma_w**2))**self.p_w
-            - ((self.V)**2/(2 * self.sigma_v**2))**self.p_v
+            -(((self.W) ** 2 / (2 * self.sigma_w**2)) ** self.p_w)
+            - ((self.V) ** 2 / (2 * self.sigma_v**2)) ** self.p_v
         )
-
-
-
-
-
