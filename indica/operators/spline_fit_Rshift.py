8b1935f8de895806c179ed21d8eb59bd41f814f3
marcosertoli <74655352+marcosertoli@users.noreply.github.com>
2023-12-18 17:21:07 +0000

    Marcosertoli/spline fit rshift (#302)
    * Rough spline-fit adaptation including searching for Rshift to get best HFS and LFS data match in fit
    
    * Cleaned up and given user capability of changing Rshift bounds
    
    * ..had forgotten to run precommit...
    
    * Added capability to NOT fit the Rshift or impose one in input
    
    * Reinstated calculation of Zeff for both LOS-averaged and profiles using inversion. Bayesian inference workflow to be reimplemented with most recent BDA workflow structure.
    
    * No previous capability compromised --> merging in without further review so this can be picked up and refined by others.
    
    ---------
    
    Co-authored-by: Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>

---
+++ indica/operators/spline_fit_Rshift.py
@@ -0,0 +1,194 @@
+from copy import deepcopy
+
+import matplotlib.pylab as plt
+import numpy as np
+from scipy.interpolate import CubicSpline
+from scipy.optimize import least_squares
+import xarray as xr
+from xarray import DataArray
+
+from indica.equilibrium import Equilibrium
+from indica.numpy_typing import ArrayLike
+from indica.readers.read_st40 import ReadST40
+
+
+def fit_profile_and_Rshift(
+    Rdata: DataArray,
+    zdata: DataArray,
+    ydata: DataArray,
+    yerr: DataArray,
+    equilibrium: Equilibrium,
+    xknots: ArrayLike = None,
+    Rshift: DataArray = None,
+    xspl: ArrayLike = np.linspace(0, 1.05, 51),
+    bc_type: str = "clamped",
+    bounds_Rshift: tuple = (-0.02, 0.02),
+    verbose: bool = False,
+):
+    """Fit a profile and the R_shift of the equilibrium"""
+
+    def residuals(all_knots):
+        if Rshift is None:
+            _Rshift = all_knots[-1]
+            yknots = all_knots[:-1]
+        else:
+            _Rshift = Rshift.sel(t=t).values
+            yknots = all_knots
+
+        _xknots, _, _ = equilibrium.flux_coords(R + _Rshift, z, t=t)
+
+        spline = CubicSpline(
+            xknots,
+            yknots,
+            axis=0,
+            bc_type=bc_type,
+        )
+        bckc = np.interp(_xknots, xspl, spline(xspl))
+
+        _residuals = (y - bckc) / err
+
+        return _residuals
+
+    # Boundary conditions
+    # values go to --> 0 outside separatrix (index = -2)
+    nknots = np.size(xknots)
+    lower_bound = np.full(nknots, -np.inf)
+    upper_bound = np.full(nknots, np.inf)
+    lower_bound[-1] = 0.0
+    upper_bound[-1] = 0.01
+    if Rshift is None:
+        # If Rshift to be fitted, add additional knot and bounds
+        nknots += 1
+        lower_bound = np.append(lower_bound, bounds_Rshift[0])
+        upper_bound = np.append(upper_bound, bounds_Rshift[1])
+
+    # Initialize DataArray that will contain the final fit result
+    yspl = xr.DataArray(
+        np.empty((len(xspl), len(ydata.t))),
+        coords=[("rho_poloidal", xspl), ("t", ydata.t.values)],
+    )
+    Rshift_fit = xr.DataArray(
+        np.empty(len(ydata.t)),
+        coords=[("t", ydata.t.values)],
+    )
+
+    all_knots = None
+    for t in ydata.t.values:
+        # Normalize data so range of parameters to scan is all similar
+        norm_factor = np.nanmax(ydata.sel(t=t).values)
+        _y = ydata.sel(t=t).values / norm_factor
+        _yerr = yerr.sel(t=t).values / norm_factor
+
+        ind = np.where(np.isfinite(_y) * np.isfinite(_yerr))[0]
+        if len(ind) > 2:
+            R = Rdata[ind]
+            z = zdata[ind]
+            y = _y[ind]
+            err = _yerr[ind]
+
+            # Initial guess: profile linearly increasing edge>core & Rshift = 0.
+            if all_knots is None:
+                all_knots = np.linspace(np.max(y), 0, np.size(xknots))
+                if Rshift is None:
+                    all_knots = np.append(all_knots, 0.0)
+
+            try:
+                fit = least_squares(
+                    residuals,
+                    all_knots,
+                    bounds=(lower_bound, upper_bound),
+                    verbose=verbose,
+                )
+
+                if Rshift is None:
+                    yknots = fit.x[:-1]
+                    _Rshift = fit.x[-1]
+                else:
+                    yknots = fit.x
+
+                all_knots = deepcopy(fit.x)
+                spline = CubicSpline(
+                    xknots,
+                    yknots,
+                    axis=0,
+                    bc_type=bc_type,
+                )
+
+                _yspl = spline(xspl) * norm_factor
+            except ValueError:
+                all_knots = None
+                _Rshift = 0.0
+                _yspl = np.full_like(xspl, 0.0)
+        else:
+            print("   bad data...")
+            _Rshift = 0.0
+            _yspl = np.full_like(xspl, 0.0)
+
+        yspl.loc[dict(t=t)] = _yspl
+        if Rshift is None:
+            Rshift_fit.loc[dict(t=t)] = _Rshift
+
+    if Rshift is not None:
+        Rshift_fit = Rshift
+
+    return yspl, Rshift_fit
+
+
+def example_run(
+    pulse: int = 11314,
+    tstart: float = 0.03,
+    tend: float = 0.13,
+    dt: float = 0.01,
+    quantity: str = "te",
+    xknots: list = None,
+    verbose: bool = False,
+):
+    st40 = ReadST40(pulse, tstart=tstart, tend=tend, dt=dt)
+    st40(["ts"])
+
+    if xknots is None:
+        if quantity == "te":
+            xknots = [0, 0.4, 0.6, 0.8, 1.1]
+        elif quantity == "ne":
+            xknots = [0, 0.4, 0.8, 0.95, 1.1]
+        else:
+            raise ValueError
+
+    data = st40.raw_data["ts"][quantity]
+    err = data.error
+    transform = data.transform
+    equilibrium = transform.equilibrium
+    R = transform.R
+    z = transform.z
+
+    fit, Rshift = fit_profile_and_Rshift(
+        R, z, data, err, equilibrium, xknots=xknots, verbose=verbose
+    )
+
+    for t in data.t:
+        _Rshift = Rshift.sel(t=t).values
+        rho, _, _ = equilibrium.flux_coords(R + _Rshift, z, t=t)
+
+        plt.ioff()
+        plt.errorbar(
+            rho,
+            data.sel(t=t),
+            err.sel(t=t),
+            marker="o",
+            label="data",
+            color="blue",
+        )
+        fit.sel(t=t).plot(linewidth=5, alpha=0.5, color="black", label="spline fit all")
+        plt.title(
+            f"pulse={pulse}, t={int(t*1000.)} ms, R_shift={(_Rshift*100.):.1f} cm"
+        )
+        plt.legend()
+        plt.show()
+
+    return data, fit
+
+
+if __name__ == "__main__":
+    plt.ioff()
+    example_run(11089, quantity="ne")
+    plt.show()
---
+++ indica/workflows/zeff_workflows.py
@@ -0,0 +1,383 @@
+from copy import deepcopy
+
+import matplotlib.pylab as plt
+import numpy as np
+import xarray as xr
+from xarray import DataArray
+
+from indica.models.diode_filters import BremsstrahlungDiode
+from indica.operators import tomo_1D
+from indica.operators.spline_fit_Rshift import fit_profile_and_Rshift
+from indica.operators.tomo_1D import SXR_tomography
+import indica.physics as ph
+from indica.readers.read_st40 import ReadST40
+from indica.utilities import set_axis_sci
+from indica.utilities import set_plot_colors
+
+PATHNAME = "./plots/"
+CM, COLS = set_plot_colors()
+
+
+def calculate_zeff(
+    pulse,
+    tstart=0.03,
+    tend=0.1,
+    dt=0.01,
+    reg_level_guess: float = 0.3,
+    fit_Rshift: bool = True,
+    plot: bool = True,
+    verbose: bool = False,
+):
+    print("Reading experimental data")
+    st40 = ReadST40(pulse, tstart, tend, dt)
+    st40(["pi", "tws_c", "ts", "efit"])
+    te_data = st40.raw_data["ts"]["te"].sel(t=slice(tstart, tend))
+    te_err = st40.raw_data["ts"]["te"].error.sel(t=slice(tstart, tend))
+    ne_data = st40.raw_data["ts"]["ne"].sel(t=slice(tstart, tend))
+    ne_err = st40.raw_data["ts"]["ne"].error.sel(t=slice(tstart, tend))
+    te_data.attrs["error"] = te_err
+    ne_data.attrs["error"] = ne_err
+
+    print("Fitting TS data")
+    te_fit, ne_fit, te_Rshift, ne_Rshift = fit_ts(
+        te_data, te_err, ne_data, ne_err, fit_Rshift=fit_Rshift, verbose=verbose
+    )
+    time = te_fit.t
+    ts_rho, _ = ne_data.transform.convert_to_rho_theta(t=time)
+    te_data.attrs["rho"] = ts_rho
+    ne_data.attrs["rho"] = ts_rho
+    te_data.attrs["Rshift"] = te_Rshift
+    ne_data.attrs["Rshift"] = ne_Rshift
+
+    print("Calculate PI spectral integral for Bremsstrahlung calculation")
+    spectra = st40.raw_data["pi"]["spectra"].interp(t=time)
+    _spectra_rho, _ = spectra.transform.convert_to_rho_theta(t=time)
+    spectra_rho = deepcopy(_spectra_rho)
+    spectra_rho.coords["channel"] = spectra.channel
+    (
+        zeff_los_avrg,
+        filter_data,
+        filter_model,
+        spectra_to_integrate,
+    ) = calculate_zeff_los_averaged(spectra, te_fit, ne_fit)
+
+    print("Calculating Zeff profile from Bremss inversion (including error)")
+    zeff_profile, tomo = calculate_zeff_profile(
+        te_fit,
+        ne_fit,
+        filter_data,
+        filter_model,
+        reg_level_guess,
+    )
+
+    if plot:
+        plot_results(
+            pulse,
+            spectra,
+            spectra_to_integrate,
+            zeff_los_avrg,
+            zeff_profile,
+            te_data,
+            ne_data,
+            te_fit,
+            ne_fit,
+            tomo,
+        )
+
+    return zeff_los_avrg, spectra
+
+
+def fit_ts(
+    te_data: DataArray,
+    te_err: DataArray,
+    ne_data: DataArray,
+    ne_err: DataArray,
+    fit_Rshift: bool = True,
+    verbose: bool = False,
+):
+    """
+    Fit TS data including (or not) an ad-hoc Rshift of scattering volume positions
+    (that can also be interpreted as a shift in the equilibrium)
+
+    N.B. Rshift calculated only for Te and then applied to Ne!!!!
+
+    Parameters
+    ----------
+    te_data - electron temperature data
+    te_err - electron temperature error
+    ne_data - electron density data
+    ne_err - electron density data
+    fit_Rshift - True if Rshift is to be fitted
+
+    Returns
+    -------
+    Te and Ne fits, and corresponding Rshift
+
+    """
+    if not fit_Rshift:
+        Rshift = xr.full_like(te_data.t, 0.0)
+    else:
+        Rshift = None
+
+    ts_R = te_data.R
+    ts_z = te_data.z
+    equilibrium = te_data.transform.equilibrium
+    print("  Te")
+    te_fit, te_Rshift = fit_profile_and_Rshift(
+        ts_R,
+        ts_z,
+        te_data,
+        te_err,
+        xknots=[0, 0.4, 0.6, 0.8, 1.1],
+        equilibrium=equilibrium,
+        Rshift=Rshift,
+        verbose=verbose,
+    )
+    print("  Ne")
+    ne_fit, ne_Rshift = fit_profile_and_Rshift(
+        ts_R,
+        ts_z,
+        ne_data,
+        ne_err,
+        xknots=[0, 0.4, 0.8, 0.95, 1.1],
+        equilibrium=equilibrium,
+        Rshift=te_Rshift,
+        verbose=verbose,
+    )
+
+    return te_fit, ne_fit, te_Rshift, ne_Rshift
+
+
+def calculate_zeff_los_averaged(
+    spectra: DataArray, te_fit: DataArray, ne_fit: DataArray
+):
+    # Interpolating Te and Ne on PI line of sight
+    _pi_rho, _ = spectra.transform.convert_to_rho_theta(t=te_fit.t)
+    pi_rho = deepcopy(_pi_rho)
+    pi_rho.coords["channel"] = spectra.channel
+    te_along_los = xr.where(pi_rho <= 1.0, te_fit.interp(rho_poloidal=pi_rho), np.nan)
+    ne_along_los = xr.where(pi_rho <= 1.0, ne_fit.interp(rho_poloidal=pi_rho), np.nan)
+
+    # Instatiating filter model and integrating spectra
+    filter_model = BremsstrahlungDiode("pi", filter_wavelength=531.7, filter_fwhm=0.6)
+    filter_model.set_los_transform(spectra.transform)
+    spectra_to_integrate, filter_data = filter_model.integrate_spectra(
+        spectra, fit_background=False
+    )
+
+    # Calculating Bremsstrahlung --> Zeff conversion along LOS & LOS-integral
+    factor = ph.zeff_bremsstrahlung(
+        te_along_los,
+        ne_along_los,
+        filter_model.filter_wavelength,
+        bremsstrahlung=xr.full_like(te_along_los, 1.0),
+        gaunt_approx="callahan",
+    )
+
+    factor_los_int = factor.sum("los_position") * filter_model.los_transform.dl
+    los_length = (
+        xr.where(np.isfinite(pi_rho), 1, 0) * filter_model.los_transform.dl
+    ).sum("los_position")
+    los_length.coords["channel"] = spectra.channel
+    zeff_los_avrg = filter_data * factor_los_int / los_length**2
+
+    return zeff_los_avrg, filter_data, filter_model, spectra_to_integrate
+
+
+def calculate_zeff_profile(
+    te_fit: DataArray,
+    ne_fit: DataArray,
+    filter_data: DataArray,
+    filter_model: BremsstrahlungDiode,
+    reg_level_guess: float = 0.3,
+):
+    has_data = np.isfinite(filter_data) * (filter_data > 0)
+    data_to_invert = filter_data.where(has_data, drop=True)
+    channels = data_to_invert.channel
+    has_data = [True] * len(channels)
+    rho_equil = filter_model.los_transform.equilibrium.rho
+    input_dict = dict(
+        brightness=data_to_invert.data,
+        brightness_error=data_to_invert.data * 0.1,
+        t=data_to_invert.t.data,
+        dl=filter_model.los_transform.dl,
+        R=filter_model.los_transform.R,
+        z=filter_model.los_transform.z,
+        rho_equil=dict(
+            R=rho_equil.R.data,
+            z=rho_equil.z.data,
+            t=rho_equil.t.data,
+            rho=rho_equil.data,
+        ),
+        has_data=has_data,
+        debug=False,
+    )
+    tomo = tomo_1D.SXR_tomography(input_dict, reg_level_guess=reg_level_guess)
+    tomo()
+
+    emissivity = DataArray(
+        tomo.emiss, coords=[("t", tomo.tvec), ("rho_poloidal", tomo.rho_grid_centers)]
+    )
+    _error = DataArray(
+        tomo.emiss_err,
+        coords=[("t", tomo.tvec), ("rho_poloidal", tomo.rho_grid_centers)],
+    )
+    emissivity.attrs["error"] = _error
+
+    wlnght = filter_model.filter_wavelength
+    _te = te_fit.interp(rho_poloidal=emissivity.rho_poloidal)
+    _ne = ne_fit.interp(rho_poloidal=emissivity.rho_poloidal)
+    _emiss = emissivity
+    _zeff = ph.zeff_bremsstrahlung(
+        _te,
+        _ne,
+        wlnght,
+        bremsstrahlung=_emiss,
+        gaunt_approx="callahan",
+    )
+    _zeff_lo = ph.zeff_bremsstrahlung(
+        _te,
+        _ne,
+        wlnght,
+        bremsstrahlung=emissivity - emissivity.error,
+        gaunt_approx="callahan",
+    )
+    _zeff_up = ph.zeff_bremsstrahlung(
+        _te,
+        _ne,
+        wlnght,
+        bremsstrahlung=emissivity + emissivity.error,
+        gaunt_approx="callahan",
+    )
+    zeff_profile = xr.where(_zeff < 10, _zeff, np.nan)
+    zeff_lo = xr.where(_zeff_lo < 10, _zeff_lo, np.nan)
+    zeff_up = xr.where(_zeff_up < 10, _zeff_up, np.nan)
+    zeff_profile.attrs["error"] = zeff_up - zeff_lo
+
+    return zeff_profile, tomo
+
+
+def plot_results(
+    pulse: int,
+    spectra: DataArray,
+    spectra_to_integrate: DataArray,
+    zeff_los_avrg: DataArray,
+    zeff_profile: DataArray,
+    te_data: DataArray,
+    ne_data: DataArray,
+    te_fit: DataArray,
+    ne_fit: DataArray,
+    tomo: SXR_tomography,
+):
+    time = zeff_profile.t
+    cols = CM(np.linspace(0.1, 0.75, len(time), dtype=float))
+    plt.figure()
+    for i, t in enumerate(time.values):
+        if i % 2:
+            zeff_profile.sel(t=t).plot(color=cols[i], label=f"t={int(t*1.e3)} ms")
+            plt.fill_between(
+                zeff_profile.rho_poloidal,
+                (zeff_profile - zeff_profile.error).sel(t=t),
+                (zeff_profile + zeff_profile.error).sel(t=t),
+                color=cols[i],
+                alpha=0.5,
+            )
+    plt.ylim(0.5, 8)
+    plt.ylabel("Zeff")
+    plt.legend()
+    plt.title(f"{pulse} Zeff profile")
+    plt.legend()
+    plt.ylim(
+        0.5,
+    )
+    ylim = plt.ylim()
+    if ylim[1] > 8:
+        plt.ylim(0.5, 8)
+
+    # Select only channels with impact parameter inside the separatrix by 1 cm
+    plt.figure()
+    Rlfs = spectra.transform.equilibrium.rmjo.interp(rho_poloidal=1, t=time)
+    Rhfs = spectra.transform.equilibrium.rmji.interp(rho_poloidal=1, t=time)
+    Rimpact = spectra.transform.impact_parameter.R
+    good_channels = ((Rimpact - 0.01) > Rhfs) * ((Rimpact + 0.01) < Rlfs)
+    good_channels.coords["channel"] = spectra.channel
+    _mean = xr.where(good_channels, zeff_los_avrg, np.nan).mean("channel", skipna=True)
+    _std = xr.where(good_channels, zeff_los_avrg, np.nan).std("channel", skipna=True)
+    _mean.plot(marker="o")
+    plt.fill_between(_mean.t, _mean - _std, _mean + _std, alpha=0.5)
+    plt.ylabel("Zeff")
+    plt.xlabel("Time (s)")
+    plt.title(f"{pulse} Zeff LOS- & channel-averaged")
+    plt.ylim(
+        0.5,
+    )
+    ylim = plt.ylim()
+    if ylim[1] > 8:
+        plt.ylim(0.5, 8)
+
+    plt.figure()
+    for i, t in enumerate(time.values):
+        if i % 2:
+            _Rshift = int(ne_data.Rshift.sel(t=t) * 100)
+            plt.errorbar(
+                ne_data.rho.sel(t=t),
+                ne_data.sel(t=t),
+                ne_data.error.sel(t=t),
+                color=cols[i],
+                marker="o",
+                label=rf"t={int(t*1.e3)} ms $\delta$R={_Rshift} cm",
+                alpha=0.6,
+            )
+    for i, t in enumerate(time.values):
+        if i % 2:
+            ne_fit.sel(t=t).plot(color=cols[i], linewidth=4, zorder=0)
+    plt.ylabel("Ne (m${-3}$)")
+    plt.xlabel("Rho-poloidal")
+    plt.title(f"{pulse} TS Ne data & fits")
+    plt.xlim(0, 1.1)
+    plt.ylim(0, np.nanmax(ne_data) * 1.1)
+    plt.legend()
+
+    plt.figure()
+    for i, t in enumerate(time.values):
+        if i % 2:
+            _Rshift = int(te_data.Rshift.sel(t=t) * 100)
+            plt.errorbar(
+                te_data.rho.sel(t=t),
+                te_data.sel(t=t),
+                te_data.error.sel(t=t),
+                color=cols[i],
+                marker="o",
+                label=rf"t={int(t*1.e3)} ms $\delta$R={_Rshift} cm",
+                alpha=0.6,
+            )
+    for i, t in enumerate(time.values):
+        if i % 2:
+            te_fit.sel(t=t).plot(color=cols[i], linewidth=4, zorder=0)
+    plt.ylabel("Te (eV)")
+    plt.xlabel("Rho-poloidal")
+    plt.title(f"{pulse} TS Te data & fits")
+    plt.xlim(0, 1.1)
+    plt.ylim(0, np.nanmax(te_data) * 1.1)
+    plt.legend()
+
+    plt.figure()
+    for i, t in enumerate(time.values):
+        if i % 2:
+            spectra_mean = spectra_to_integrate.mean("channel")
+            spectra_std = spectra_to_integrate.std("channel")
+            plt.fill_between(
+                spectra_mean.wavelength,
+                (spectra_mean - spectra_std).sel(t=t),
+                (spectra_mean + spectra_std).sel(t=t),
+                color=cols[i],
+                label=f"t={int(t*1.e3)} ms",
+                alpha=0.5,
+            )
+    plt.ylabel("(...)")
+    plt.xlabel("Wavelength (nm)")
+    plt.title(f"{pulse} PI spectra integrated")
+    set_axis_sci()
+    plt.legend()
+
+    tomo.show_reconstruction()
