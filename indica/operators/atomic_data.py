159f9d1f3e765e1804f1a69dc4e5c56df06cc046
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2024-04-17 15:51:29 +0100

    Deleted useless keyword

--- indica/operators/atomic_data.py
+++ indica/operators/atomic_data.py
@@ -901,7 +901,7 @@ def default_atomic_data(
     return fract_abu, power_loss_tot, power_loss_sxr
 
 
-def default_profiles(n_rad:int = 100, rho_lin:bool=False):
+def default_profiles(n_rad: int = 100):
     """
     Set default plasma profiles to calculate atomic data
     """
