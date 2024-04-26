0a55f9926f5f4d81f0e39e6306db47f64f91bcd7
michael gemmell <michael.gemmell@tokamakenergy.co.uk>
2024-04-26 09:25:11 +0100

    readded bda_run test

--- tests/integration/workflows/test_bda_run.py
+++ tests/integration/workflows/test_bda_run.py
@@ -5,15 +5,13 @@ class TestBDARun:
     """
     Mock Equilibrium is broken in reader!
     """
-    def teststub(self):
-        return
-    # def test_bda_run(self):
-    #     config_path = "indica.workflows.configs.example_bda"
-    #     config_file = importlib.import_module(config_path)
-    #     bda_workflow = bda_run(**config_file.__dict__)
+    def test_bda_run(self):
+        config_path = "indica.workflows.configs.test_bda"
+        config_file = importlib.import_module(config_path)
+        bda_workflow = bda_run(**config_file.__dict__)
 
 
 if __name__ == "__main__":
 
     test = TestBDARun()
-    # test.test_bda_run()
+    test.test_bda_run()
