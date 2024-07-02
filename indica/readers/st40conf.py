a2287d72f7b10a526fd451ff679f99138051e665
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2024-07-02 14:10:49 +0100

    Fixed revision reading across all methods

--- indica/readers/st40conf.py
+++ indica/readers/st40conf.py
@@ -157,20 +157,12 @@ class ST40Conf:
                 "R_shift": ".global:rshift",
             },
             "astra": {
-                "f": ".profiles.psi_norm:fpol",
                 "faxs": ".global:faxs",
                 "fbnd": ".global:fbnd",
-                "ftor": ".profiles.psi_norm:ftor",
-                "psi_1d": ".profiles.psi_norm:psi",
-                "psi": ".psi2d:psi",
-                "volume": ".profiles.psi_norm:volume",
-                "area": ".profiles.psi_norm:areat",
                 "rmag": ".global:rmag",
                 "rgeo": ".global:rgeo",
                 "zmag": ".global:zmag",
                 "zgeo": ".global:zgeo",
-                "rbnd": ".p_boundary:rbnd",
-                "zbnd": ".p_boundary:zbnd",
                 "wp": ".global:wth",
                 "ipla": ".global:ipl",
                 "upl": ".global:upl",
@@ -178,6 +170,20 @@ class ST40Conf:
                 "wtherm": ".global:wtherm",
                 "wfast": ".global:wfast",
                 "df": ".global.df",
+                "pnb": ".global:pnb",  # W
+                "pabs": ".global:pabs",  # W
+                "p_oh": ".global:p_oh",  # W
+                "q": ".profiles.psi_norm:q",
+                "f": ".profiles.psi_norm:fpol",
+                "ftor": ".profiles.psi_norm:ftor",
+                "psi_1d": ".profiles.psi_norm:psi",
+                "p": ".profiles.psi_norm:p",
+                "volume": ".profiles.psi_norm:volume",
+                "area": ".profiles.psi_norm:areat",
+                "sigmapar": ".profiles.psi_norm:sigmapar",  # 1/(Ohm*m)
+                "psi": ".psi2d:psi",
+                "rbnd": ".p_boundary:rbnd",
+                "zbnd": ".p_boundary:zbnd",
                 "elon": ".profiles.astra:elon",  #
                 "j_bs": ".profiles.astra:j_bs",  # MA/m2
                 "j_nbi": ".profiles.astra:j_nbi",  # MA/m2
@@ -197,7 +203,7 @@ class ST40Conf:
                 "qnbi": ".profiles.astra:qnbi",  # MW/m3
                 "q_oh": ".profiles.astra:q_oh",  # MW/m3
                 "q_rf": ".profiles.astra:q_rf",  # MW/m3
-                "rho": ".profiles.astra:rho",  # ASTRA rho-toroidal
+                "rhot": ".profiles.astra:rho",  # ASTRA rho-toroidal
                 "rmid": ".profiles.astra:rmid",  # Centre of flux surfaces, m
                 "rminor": ".profiles.astra:rminor",  # minor radius, m
                 "sbm": ".profiles.astra:sbm",  # 10^19/m^3/s
@@ -210,15 +216,8 @@ class ST40Conf:
                 "t_d": ".profiles.astra:t_d",  # keV
                 "t_t": ".profiles.astra:t_t",  # keV
                 "zeff": ".profiles.astra:zeff",
-                "areat": ".profiles.psi_norm:areat",  # Toroidal cross section, m2
-                "p": ".profiles.psi_norm:p",
                 "pblon": ".profiles.astra:pblon",
                 "pbper": ".profiles.astra:pbper",
-                "pnb": ".global:pnb",  # W
-                "pabs": ".global:pabs",  # W
-                "p_oh": ".global:p_oh",  # W
-                "q": ".profiles.psi_norm:q",
-                "sigmapar": ".profiles.psi_norm:sigmapar",  # 1/(Ohm*m)
                 "nn": ".profiles.astra:nn",  # 10^19/m^3
                 "niz1": ".profiles.astra:niz1",  # 10^19/m^3
                 "niz2": ".profiles.astra:niz2",  # 10^19/m^3
