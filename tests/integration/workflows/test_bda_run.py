2d959f5b13da0499cfb8dcc61e8202f751822b73
michael gemmell <michael.gemmell@tokamakenergy.co.uk>
2024-07-31 15:42:36 +0100

    using absolute path for tests

--- tests/integration/workflows/test_bda_run.py
+++ tests/integration/workflows/test_bda_run.py
@@ -1,15 +1,22 @@
-from hydra import compose
-from hydra import initialize
+from pathlib import Path
 
+from hydra import compose
+from hydra import initialize_config_dir
+
+import indica
 from indica.workflows.bda_run import bda_run
 
+PROJECT_PATH = Path(indica.__file__)
+CONFIG_PATH = f"{PROJECT_PATH}/../configs"
 
+print(CONFIG_PATH)
 class TestBDARun:
     def test_bda_run(
         self,
     ):
-        with initialize(
-            version_base=None, config_path="../../../indica/configs/workflows/bda_run"
+        print(f"{CONFIG_PATH}/workflows/bda_run")
+        with initialize_config_dir(
+            version_base=None, config_dir=f"{CONFIG_PATH}/workflows/bda_run"
         ):
             cfg = compose(
                 config_name="mock_run",
