3df9f390ae052c9d94caa96e9da86fc0e5634b55
michael gemmell <michael.gemmell@tokamakenergy.co.uk>
2024-07-31 14:12:43 +0100

    precommit

--- tests/integration/workflows/test_bda_run.py
+++ tests/integration/workflows/test_bda_run.py
@@ -1,11 +1,19 @@
-from hydra import compose, initialize
+from hydra import compose
+from hydra import initialize
+
 from indica.workflows.bda_run import bda_run
 
 
 class TestBDARun:
-    def test_bda_run(self, ):
-        with initialize(version_base=None, config_path="../../../indica/configs/workflows/bda_run"):
-            cfg = compose(config_name="mock_run", overrides=["write_info.mds_write=False"])
+    def test_bda_run(
+        self,
+    ):
+        with initialize(
+            version_base=None, config_path="../../../indica/configs/workflows/bda_run"
+        ):
+            cfg = compose(
+                config_name="mock_run", overrides=["write_info.mds_write=False"]
+            )
         bda_run(cfg)
 
 
