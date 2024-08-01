1d1c5a8ef7437ab645b57114f5c5d0f742b144fb
michael gemmell <michael.gemmell@tokamakenergy.co.uk>
2024-08-01 14:45:57 +0100

    precommit

--- tests/integration/workflows/test_priors.py
+++ tests/integration/workflows/test_priors.py
@@ -1,27 +1,29 @@
+from hydra import compose
+from hydra import initialize_config_module
 
-from hydra import compose, initialize_config_module
-from indica.workflows.priors import PriorManager, sample_from_priors
+from indica.workflows.priors import PriorManager
+from indica.workflows.priors import sample_from_priors
 
 
 class TestPriors:
     def setup_class(self):
         with initialize_config_module(
-            version_base=None, config_module = "indica.configs.workflows.priors"
+            version_base=None, config_module="indica.configs.workflows.priors"
         ):
             self.cfg = compose(config_name="config")
 
     def test_prior_manager_initalises_with_config(self):
-        pm = PriorManager(**self.cfg)
+        PriorManager(**self.cfg)
         assert True
 
     def test_prior_evaluates(self):
         pm = PriorManager(**self.cfg)
-        prior = pm.ln_prior({"electron_density.y0": 1e20, "electron_density.y1": 1e19})
+        pm.ln_prior({"electron_density.y0": 1e20, "electron_density.y1": 1e19})
         assert True
 
     def test_sampling_from_priors(self):
         pm = PriorManager(**self.cfg)
-        samples = sample_from_priors(
+        sample_from_priors(
             ["electron_density.y0", "electron_density.y1"], pm.priors
         )
         assert True
