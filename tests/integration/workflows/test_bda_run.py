5dc0d5cad174d65b9699d251a05533af8040a9b9
michael gemmell <michael.gemmell@tokamakenergy.co.uk>
2024-08-01 10:48:18 +0100

    fixed testing configs not finding the correct path

--- tests/integration/workflows/test_bda_run.py
+++ tests/integration/workflows/test_bda_run.py
@@ -1,28 +1,24 @@
-from pathlib import Path
 
 from hydra import compose
-from hydra import initialize_config_dir
+from hydra import initialize_config_dir, initialize_config_module, initialize
 
-import indica
 from indica.workflows.bda_run import bda_run
 
-PROJECT_PATH = Path(indica.__file__)
-CONFIG_PATH = f"{PROJECT_PATH}/../configs"
 
 # TODO: Can't get configs to work properly in CI/CD
-# class TestBDARun:
-#     def test_bda_run(
-#         self,
-#     ):
-#         with initialize_config_dir(
-#             version_base=None, config_dir=f"{CONFIG_PATH}/workflows/bda_run"
-#         ):
-#             cfg = compose(
-#                 config_name="mock_run",
-#             )
-#         bda_run(cfg)
-#
-#
-# if __name__ == "__main__":
-#     test = TestBDARun()
-#     test.test_bda_run()
+class TestBDARun:
+    def test_bda_run(
+        self,
+    ):
+        with initialize_config_module(
+            version_base=None, config_module="indica.configs.workflows.bda_run"
+        ):
+            cfg = compose(
+                config_name="mock_run",
+            )
+        bda_run(cfg)
+
+
+if __name__ == "__main__":
+    test = TestBDARun()
+    test.test_bda_run()
