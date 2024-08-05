b221512e193c96c92a3af3c42e3b558eb0c41c46
michael gemmell <michael.gemmell@tokamakenergy.co.uk>
2024-08-05 13:54:41 +0100

    tests for __call__ added

--- tests/integration/workflows/test_model_coordinator.py
+++ tests/integration/workflows/test_model_coordinator.py
@@ -19,6 +19,16 @@ def initialise_model_coordinator(model_settings=None):
     return model_coordinator
 
 
+def initialise_model_coordinator_and_setup(
+    plasma, transforms, equilibrium, model_settings=None
+):
+    model_coordinator = initialise_model_coordinator(model_settings=model_settings)
+    model_coordinator.set_plasma(plasma)
+    model_coordinator.set_transforms(transforms)
+    model_coordinator.set_equilibrium(equilibrium)
+    return model_coordinator
+
+
 class TestModelCoordinator:
     def setup_class(self):
         self.plasma = load_default_objects("st40", "plasma")
@@ -55,24 +65,44 @@ class TestModelCoordinator:
         for model_name, model in model_coordinator.models.items():
             assert hasattr(model.transform, "equilibrium")
 
-    def test_get(self):
-        model_coordinator = initialise_model_coordinator()
-        model_coordinator.set_plasma(self.plasma)
-        model_coordinator.set_transforms(self.transforms)
-        model_coordinator.set_equilibrium(self.equilibrium)
-
+    def test_get_with_all_model_names(self):
+        model_coordinator = initialise_model_coordinator_and_setup(
+            self.plasma, self.transforms, self.equilibrium
+        )
         for model_name, model in model_coordinator.models.items():
             assert model_coordinator.get(model_name)
 
     def test_get_with_wrong_name(self):
-        model_coordinator = initialise_model_coordinator()
-        model_coordinator.set_plasma(self.plasma)
-        model_coordinator.set_transforms(self.transforms)
-        model_coordinator.set_equilibrium(self.equilibrium)
+        model_coordinator = initialise_model_coordinator_and_setup(
+            self.plasma, self.transforms, self.equilibrium
+        )
         assert not model_coordinator.get("dummy")
+
+    def test_default_call(self):
+        model_coordinator = initialise_model_coordinator_and_setup(
+            self.plasma, self.transforms, self.equilibrium
+        )
+        model_coordinator()
+        assert model_coordinator.binned_data
+
+    def test_call_with_nested_kwargs(self):
+        model_coordinator = initialise_model_coordinator_and_setup(
+            self.plasma, self.transforms, self.equilibrium
+        )
+        model_coordinator(["xrcs"], **{"xrcs": {"background": 0}})
+        assert model_coordinator.binned_data.get("xrcs", {})
+        assert model_coordinator.call_kwargs.get("xrcs", {}) == {"background": 0}
+
+    def test_call_with_flat_kwargs(self):
+        model_coordinator = initialise_model_coordinator_and_setup(
+            self.plasma, self.transforms, self.equilibrium
+        )
+        model_coordinator(["xrcs"], flat_kwargs={"xrcs.background": 0})
+        assert model_coordinator.binned_data.get("xrcs", {})
+        assert model_coordinator.call_kwargs.get("xrcs", {}) == {"background": 0}
 
 
 if __name__ == "__main__":
     test = TestModelCoordinator()
     test.setup_class()
-    test.test_get_with_wrong_name()
+    test.test_call_with_flat_kwargs()
