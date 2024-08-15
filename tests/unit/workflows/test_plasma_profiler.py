c8e65b0de8677cb5811886653449a147c3c309b3
michael gemmell <michael.gemmell@tokamakenergy.co.uk>
2024-08-15 15:11:16 +0100

    precommit

--- tests/unit/workflows/test_plasma_profiler.py
+++ tests/unit/workflows/test_plasma_profiler.py
@@ -2,7 +2,8 @@ import pytest
 
 from indica.defaults.load_defaults import load_default_objects
 from indica.profilers import ProfilerGauss
-from indica.workflows.plasma_profiler import PlasmaProfiler, PLASMA_ATTRIBUTE_NAMES
+from indica.workflows.plasma_profiler import PLASMA_ATTRIBUTE_NAMES
+from indica.workflows.plasma_profiler import PlasmaProfiler
 
 
 class TestPlasmaProfiler:
@@ -13,7 +14,11 @@ class TestPlasmaProfiler:
                 datatype=profile_name.split(":")[0],
                 xspl=self.plasma.rho,
             )
-            for profile_name in ["electron_density", "ion_temperature", "impurity_density:ar"]
+            for profile_name in [
+                "electron_density",
+                "ion_temperature",
+                "impurity_density:ar",
+            ]
         }
         self.plasma_profiler = PlasmaProfiler(
             plasma=self.plasma,
@@ -31,11 +36,12 @@ class TestPlasmaProfiler:
         assert all(self.plasma.electron_density.sel(rho_poloidal=0) == 1.02e19)
 
         self.plasma_profiler({"impurity_density:ar.y0": 1.02e19})
-        assert all(self.plasma.impurity_density.sel(element="ar", rho_poloidal=0) == 1.02e19)
+        assert all(
+            self.plasma.impurity_density.sel(element="ar", rho_poloidal=0) == 1.02e19
+        )
 
     def test_plasma_attribute_names_is_default(self):
         assert self.plasma_profiler.plasma_attribute_names == PLASMA_ATTRIBUTE_NAMES
-
 
     def test_change_plasma_profiles_outside_time_range_fails(self):
         with pytest.raises(KeyError):
