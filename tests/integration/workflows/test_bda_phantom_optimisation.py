d6200009705d1acc07db5303217dc103db48ee5a
michael gemmell <michael.gemmell@tokamakenergy.co.uk>
2024-11-07 10:36:19 +0000

    bda tests readded

---
+++ tests/integration/workflows/test_bda_phantom_optimisation.py
@@ -0,0 +1,25 @@
+from hydra import compose
+from hydra import initialize_config_module
+
+from indica.workflows.bda_phantom_optimisation import bda_phantom_optimisation
+
+
+class TestBDARun:
+    def test_emcee_run(
+        self,
+    ):
+        with initialize_config_module(version_base=None, config_module="configs.workflows.bda"):
+            cfg = compose(
+                config_name="test_emcee",
+            )
+        bda_phantom_optimisation(cfg)
+
+    def test_bo_run(
+        self,
+    ):
+        with initialize_config_module(version_base=None, config_module="configs.workflows.bda"):
+            cfg = compose(
+                config_name="test_bo",
+            )
+        bda_phantom_optimisation(cfg)
+
