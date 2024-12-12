6ae4e708835a535c0aa2120e453743cab4599452
michael gemmell <michael.gemmell@tokamakenergy.co.uk>
2024-12-12 16:01:29 +0000

    paths fixing

--- indica/configs/readers/__init__.py
+++ indica/configs/readers/__init__.py
@@ -1,6 +1,6 @@
 from .jetconf import JETConf
 from .st40conf import ST40Conf
-from .st40readerprocessorconf import ST40ReaderProcessorConf
+from indica.configs.readers.st40readerprocessorconf import ST40ReaderProcessorConf
 
 __all__ = [
     "ST40Conf",
