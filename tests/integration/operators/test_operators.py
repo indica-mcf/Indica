463c6c955f6063cf3094322deff6496ac490b3ae
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2024-12-18 13:59:59 +0000

    Moved all operator testing in 1 file

---
+++ tests/integration/operators/test_operators.py
@@ -0,0 +1,24 @@
+from indica.examples.example_operators import example_tomo_1D, example_tomo_asymmetry, example_fit_ts
+
+# TODO: expand testing to test output of operators, not just that they run ;-)!!
+
+
+def test_tomo_1D_with_asymmetry():
+    _ = example_tomo_1D(asymmetric_profile=True, plot=False)
+
+
+def test_tomo_1D_with_symmetry():
+
+    _ = example_tomo_1D(asymmetric_profile=False, plot=False)
+
+def test_tomo_asymmetry_with_asymmetry():
+
+    _ = example_tomo_asymmetry(asymmetric_profile=True, plot=False)
+
+def test_tomo_asymmetry_with_symmetry():
+
+    _ = example_tomo_asymmetry(asymmetric_profile=False, plot=False)
+
+def test_fit_ts():
+
+    _ = example_fit_ts(plot=False)
