95f9b5e5ce75a8a64dc42e17f42753df8fc703cf
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2026-03-23 17:32:53 +0000

    Deleted beam_utils and saved all the utilities to the child Nbi operator class

--- indica/operators/beam_utils/adas_nbi_utils.py
+++
@@ -1,9 +0,0 @@
-"""Utilities for running the ADAS NBI model."""
-
-
-def run_adas(operator, ctx: dict) -> dict:
-    raise NotImplementedError("ADAS NBI model is not implemented yet.")
-
-
-# Backward compatibility alias.
-_run_adas = run_adas
