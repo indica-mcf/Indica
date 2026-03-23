88ab032f3c48d160b38d1efaf17ec7f892e6192e
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2026-03-23 17:32:53 +0000

    Deleted beam_utils and saved all the utilities to the child Nbi operator class

--- indica/operators/beam_utils/analytic_nbi_utils.py
+++
@@ -1,9 +0,0 @@
-"""Utilities for running the analytic NBI model."""
-
-
-def run_analytic(operator, ctx: dict) -> dict:
-    raise NotImplementedError("ANALYTIC NBI model is not implemented yet.")
-
-
-# Backward compatibility alias.
-_run_analytic = run_analytic
