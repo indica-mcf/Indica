59afe08b21907abac034bee7dc7c082011d043c4
Jussi Hakosalo <jussi.hakosalo@protonmail.com>
2026-07-13 14:18:35 +0100

    feat: eq expansion as a component

--- indica/workflows/jussiphd/components/data/__init__.py
+++ indica/workflows/jussiphd/components/data/__init__.py
@@ -7,6 +7,11 @@ from .real_dataset_generation import generate_and_save_real_dataset
 from .real_dataset_generation import generate_and_save_real_multipulse_dataset
 from .real_dataset_generation import load_real_transform_from_pulse
 from .quality_filtering import filter_dataset_csv_slices
+from .expanded_equilibria_generation import (
+    build_sampled_plasma_expanded_equilibria_dataset,
+    expand_brightness_with_equilibria,
+    save_equilibrium_plots,
+)
 
 __all__ = [
     "generate_and_save_real_multipulse_brightness_dataset",
@@ -14,4 +19,7 @@ __all__ = [
     "generate_and_save_real_multipulse_dataset",
     "load_real_transform_from_pulse",
     "filter_dataset_csv_slices",
+    "expand_brightness_with_equilibria",
+    "build_sampled_plasma_expanded_equilibria_dataset",
+    "save_equilibrium_plots",
 ]
