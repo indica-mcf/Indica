ab226df7bdede83f6e15b386b7c6788eb8223f45
Marco Sertoli <marco.sertoli@tokamakenergy.co.uk>
2026-07-24 15:18:23 +0100

    Init for profilers

---
+++ indica/profilers/__init__.py
@@ -0,0 +1,5 @@
+from .profiler_gauss import ProfilerGauss
+from .profiler_spline import ProfilerCubicSpline
+from .profiler_spline import ProfilerMonoSpline
+
+__all__ = ["ProfilerGauss", "ProfilerCubicSpline", "ProfilerMonoSpline"]
