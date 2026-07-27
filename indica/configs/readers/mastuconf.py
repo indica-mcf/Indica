9c43f35203d0512ce6ea0d7e3d885cd1f5fdbef3
Beckley, Christopher <christopher.beckley@ukaea.uk>
2026-07-27 14:23:33 +0100

    Start largely empty mastuconfig

---
+++ indica/configs/readers/mastuconf.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+from indica.configs.readers.machineconf import MachineConf
+
+class MASTUConf(MachineConf):
+    def __init__(self):
+        self.MACHINE_DIMS = ((0.1, 2.0), (-2.2, 2.2))
+        self.INSTRUMENT_METHODS = {}
+        self.QUANTITIES_PATH = {}
+
