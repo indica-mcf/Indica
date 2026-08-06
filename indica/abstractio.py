603cfcdc77056493b777b8e3916a9e0e5ce8a229
Evie Litherland-Smith <evie.litherland-smith@ukaea.uk>
2026-08-06 15:38:29 +0100

    Remove abstract methods so it doesn't break ADAS reader

--- indica/abstractio.py
+++ indica/abstractio.py
@@ -2,13 +2,7 @@
 
 from abc import ABC
 from abc import abstractmethod
-from typing import List
 from typing import Literal
-from typing import Tuple
-
-import numpy as np
-
-from indica.numpy_typing import RevisionLike
 
 
 class BaseIO(ABC):
@@ -69,39 +63,4 @@ class BaseIO(ABC):
         etc.) to which data is being written."""
         raise NotImplementedError(
             "{} does not implement a 'close' method.".format(self.__class__.__name__)
-        )
-
-    @abstractmethod
-    def get_data(
-        self, uid: str, instrument: str, quantity: str, revision: RevisionLike
-    ) -> Tuple[np.array, List[np.array], str, str]:
-        """Fetch a quantity, it's coordinates, and it's units from a database."""
-        raise NotImplementedError(
-            "{} does not implement a 'get_data' method.".format(self.__class__.__name__)
-        )
-
-    @abstractmethod
-    def get_error(
-        self, uid: str, instrument: str, quantity: str, revision: RevisionLike
-    ) -> Tuple[np.array, List[np.array], str, str]:
-        """Fetch the errors associated with a quantity from a database."""
-        raise NotImplementedError(
-            "{} does not implement a 'get_error' method.".format(
-                self.__class__.__name__
-            )
-        )
-
-    @abstractmethod
-    def get_revision(
-        self, uid: str, instrument: str, revision: RevisionLike
-    ) -> tuple[RevisionLike, bool]:
-        """Get actual revision that's being read from database, converts relative
-        revision (e.g. 0, latest) to absolute, and return if latest/best revision
-        according to source.
-
-        """
-        raise NotImplementedError(
-            "{} does not implement a 'get_revision' method.".format(
-                self.__class__.__name__
-            )
         )
